b4cc434552534e9986cefbcb50dbd52396568e11
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2023-07-05 04:12:26 -0400

    Hide alerts via SCSS

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -644,7 +644,7 @@ export default class App extends React.PureComponent {
         this.setState({ "session": nextSession }, () => {
             const { session } = this.state;
             if (session === false && existingSession === true){
-                // Alerts.queue(Alerts.LoggedOut); // TODO: re-enable once authentication is implemented
+                Alerts.queue(Alerts.LoggedOut);
                 // Clear out remaining auth/JWT stuff from localStorage if any.
                 JWT.remove();
             } else if (session === true && existingSession === false){
