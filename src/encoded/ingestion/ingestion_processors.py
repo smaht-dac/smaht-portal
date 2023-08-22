73a30f8337f16daa46ec53e7754709970eef5af2
David Michaels <david_michaels@hms.harvard.edu>
2023-08-22 10:14:22 -0400

    Continued ingester work.

--- src/encoded/ingestion/ingestion_processors.py
+++ src/encoded/ingestion/ingestion_processors.py
@@ -6,8 +6,7 @@ from snovault.util import debuglog, s3_local_file
 from snovault.ingestion.common import get_parameter
 from snovault.ingestion.ingestion_processors import ingestion_processor
 from snovault.types.ingestion import SubmissionFolio
-#from ..submit import submit_metadata_bundle
-#from ..submit_genelist import submit_genelist, submit_variant_update
+from dcicutils.sheet_utils import ItemManager
 
 
 log = structlog.getLogger(__name__)
@@ -25,3 +24,21 @@ def handle_metadata_bundle(submission: SubmissionFolio):
 
         s3_client = submission.s3_client
         submission_id = submission.submission_id
+        consortium = get_parameter(submission.parameters, 'consortium')
+        submission_center = get_parameter(submission.parameters, 'submission_center')
+        validate_only = get_parameter(
+            submission.parameters,
+            'validate_only',
+            as_type=bool,
+            default=False
+        )
+        s3_bucket = submission.bucket
+        s3_key = submission.object_name
+        with s3_local_file(s3_client, bucket=s3_bucket, key=s3_key) as filename:
+            file_items = ItemManager.load_workbook(filename)
+            do_something_with_this_loaded_spreadsheet(file_items)
+
+
+def do_something_with_this_loaded_spreadsheet(items: dict) -> None:
+    import pdb ; pdb.set_trace()
+    pass
