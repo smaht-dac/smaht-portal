5697c4911a655947f61594163ad0478beae5fdac
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2023-09-20 12:51:11 -0400

    prettier bulk reformat

--- src/encoded/static/components/browse/columnExtensionMap.js
+++ src/encoded/static/components/browse/columnExtensionMap.js
@@ -5,7 +5,7 @@ import _ from 'underscore';
 
 import { basicColumnExtensionMap } from '@hms-dbmi-bgm/shared-portal-components/es/components/browse/components/table-commons';
 
-export const DEFAULT_WIDTH_MAP = { 'lg' : 200, 'md' : 180, 'sm' : 120, 'xs' : 120 };
+export const DEFAULT_WIDTH_MAP = { lg: 200, md: 180, sm: 120, xs: 120 };
 
 export const columnExtensionMap = {
     ...basicColumnExtensionMap,
--- src/encoded/static/components/index.js
+++ src/encoded/static/components/index.js
@@ -10,52 +10,50 @@
  * Individual item-type-view files might themselves register a PageTitle panel view.
  */
 
-import { content_views }                    from './globals';
+import { content_views } from './globals';
 
-import StaticPage                           from './static-pages/StaticPage';
-import DirectoryPage                        from './static-pages/DirectoryPage';
+import StaticPage from './static-pages/StaticPage';
+import DirectoryPage from './static-pages/DirectoryPage';
 
-import HomePage                             from './static-pages/HomePage';
+import HomePage from './static-pages/HomePage';
 
-
-import DefaultItemView                      from './item-pages/DefaultItemView';
-import HealthView                           from './item-pages/HealthView';
-import UserView, { ImpersonateUserForm }    from './item-pages/UserView';
-import SchemaView                           from './item-pages/SchemaView';
-import FallbackView                         from './item-pages/FallbackView';
-import DocumentView                         from './item-pages/DocumentView';
-import StaticSectionView                    from './item-pages/StaticSectionView';
-import SMaHTSubmissionView                  from './forms/SMaHTSubmissionView';
-import SearchView                           from './browse/SearchView';
+import DefaultItemView from './item-pages/DefaultItemView';
+import HealthView from './item-pages/HealthView';
+import UserView, { ImpersonateUserForm } from './item-pages/UserView';
+import SchemaView from './item-pages/SchemaView';
+import FallbackView from './item-pages/FallbackView';
+import DocumentView from './item-pages/DocumentView';
+import StaticSectionView from './item-pages/StaticSectionView';
+import SMaHTSubmissionView from './forms/SMaHTSubmissionView';
+import SearchView from './browse/SearchView';
 
 /**
  * These content_view.register actions occur in this index.js as otherwise
  * the item-type-view files might not be included in the compiled build.js
  * due to webpack/babel tree-shaking config/plugins.
  */
-content_views.register(StaticPage,                  'StaticPage');
-content_views.register(DirectoryPage,               'DirectoryPage');
+content_views.register(StaticPage, 'StaticPage');
+content_views.register(DirectoryPage, 'DirectoryPage');
 
-content_views.register(HomePage,                    'HomePage');
+content_views.register(HomePage, 'HomePage');
 
-content_views.register(DefaultItemView,             'Item');
-content_views.register(HealthView,                  'Health');
-content_views.register(DocumentView,                'Document');
-content_views.register(DocumentView,                'Image');
-content_views.register(SchemaView,                  'JSONSchema');
-content_views.register(UserView,                    'User');
-content_views.register(ImpersonateUserForm,         'User', 'impersonate-user');
-content_views.register(StaticSectionView,           'StaticSection');
+content_views.register(DefaultItemView, 'Item');
+content_views.register(HealthView, 'Health');
+content_views.register(DocumentView, 'Document');
+content_views.register(DocumentView, 'Image');
+content_views.register(SchemaView, 'JSONSchema');
+content_views.register(UserView, 'User');
+content_views.register(ImpersonateUserForm, 'User', 'impersonate-user');
+content_views.register(StaticSectionView, 'StaticSection');
 
-content_views.register(SMaHTSubmissionView,         'Item', 'edit');
-content_views.register(SMaHTSubmissionView,         'Item', 'create');
-content_views.register(SMaHTSubmissionView,         'Item', 'clone');
-content_views.register(SMaHTSubmissionView,         'Search', 'add');
+content_views.register(SMaHTSubmissionView, 'Item', 'edit');
+content_views.register(SMaHTSubmissionView, 'Item', 'create');
+content_views.register(SMaHTSubmissionView, 'Item', 'clone');
+content_views.register(SMaHTSubmissionView, 'Search', 'add');
 
-content_views.register(SearchView,                  'Search');
-content_views.register(SearchView,                  'Search', 'selection');
-content_views.register(SearchView,                  'Search', 'multiselect');
-
+content_views.register(SearchView, 'Search');
+content_views.register(SearchView, 'Search', 'selection');
+content_views.register(SearchView, 'Search', 'multiselect');
 
 // Fallback for anything we haven't registered
 content_views.fallback = function () {
