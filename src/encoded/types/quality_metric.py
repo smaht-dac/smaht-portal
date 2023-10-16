278560045d3c78d57980d2bdbc81e75dcaf7edec
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-10-16 14:24:46 -0400

    Add QualityMetric type file back

--- src/encoded/types/quality_metric.py
+++ src/encoded/types/quality_metric.py
@@ -1,15 +1,15 @@
-from encoded_core.types.quality_metric import QualityMetric as CoreQualityMetric
-from snovault import abstract_collection, load_schema
+from encoded_core.types.quality_metric import QualityMetricGeneric as CoreQualityMetricGeneric
+from snovault import collection, load_schema
 
 from .base import Item as SMAHTItem
 
 
-@abstract_collection(
+@collection(
     name='quality-metrics',
     properties={
         'title': 'Quality Metrics',
         'description': 'Listing of quality metrics',
     })
-class QualityMetric(SMAHTItem, CoreQualityMetric):
+class QualityMetric(SMAHTItem, CoreQualityMetricGeneric):
     item_type = 'quality_metric'
     schema = load_schema("encoded:schemas/quality_metric.json")
