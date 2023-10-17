4cc7fb9f46c8064a4be7347edd69a6c98ebd5bd9
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2023-10-17 16:31:51 -0400

    Replace Mada font with Inter

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -1507,7 +1507,7 @@ export default class App extends React.PureComponent {
                         crossOrigin="true"
                     />
                     <link
-                        href="https://fonts.googleapis.com/css2?family=Mada:wght@300;400&family=Montserrat:wght@800&display=swap"
+                        href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Montserrat:wght@800&display=swap"
                         rel="stylesheet"
                     />
                     {/* Can set webpack.config.js browser build's externals "react":"React" and load via CDN but need to then allow cross-origin requests to CDN domain
