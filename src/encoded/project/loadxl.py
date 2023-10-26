a84d6414f6e56b31237a4104f4d73b8cd1b49ba0
David Michaels <david_michaels@hms.harvard.edu>
2023-10-26 10:54:18 -0400

    changed camel to snake case for loadxl ordering items

--- src/encoded/project/loadxl.py
+++ src/encoded/project/loadxl.py
@@ -3,27 +3,28 @@ from snovault.project.loadxl import SnovaultProjectLoadxl
 
 class SMaHTProjectLoadxl(SnovaultProjectLoadxl):
 
+    # N.B. snovault.loadxl expects to get this in snake-case (not camel-case) format.
     order = [
-            'AccessKey',
-            'User',
-            'Consortium',
-            'SubmissionCenter',
-            'FileFormat',
-            'QualityMetric',
-            'OutputFile',
-            'ReferenceFile',
-            'Software',
-            'Workflow',
-            'WorkflowRun',
-            'MetaWorkflow',
-            'MetaWorkflowRun',
-            'Image',
-            'Document',
-            'StaticSection',
-            'Page',
-            'FilterSet',
-            'HiglassViewConfig',
-            'IngestionSubmission'
+            'consortium',
+            'submission_center',
+            'access_key',
+            'user',
+            'file_format',
+            'quality_metric',
+            'output_file',
+            'reference_file',
+            'software',
+            'workflow',
+            'workflow_run',
+            'meta_workflow',
+            'meta_workflow_run',
+            'image',
+            'document',
+            'static_section',
+            'page',
+            'filter_set',
+            'higlass_view_config',
+            'ingestion_submission'
     ]
 
     def loadxl_order(self):
