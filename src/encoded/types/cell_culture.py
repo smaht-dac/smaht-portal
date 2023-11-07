84d58c8a3d55ef6636261e18a384a7140c4815b0
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2023-11-07 17:29:58 -0500

    Add cell culture related items
    Also, share more among sample item types.

---
+++ src/encoded/types/cell_culture.py
@@ -0,0 +1,17 @@
+from snovault import collection, load_schema
+
+from .base import Item as SmahtItem
+
+
+@collection(
+    name="cell-cultures",
+    unique_key="cell_culture:submitted_id",
+    properties={
+        "title": "Cell Cultures",
+        "description": "Conditions for growing cells",
+    },
+)
+class CellCulture(SmahtItem):
+    item_type = "cell_culture"
+    schema = load_schema("encoded:schemas/cell_culture.json")
+    embedded_list = []
