c3f4a496135a713316beb563643387dfd8bf3eae
David Michaels <david_michaels@hms.harvard.edu>
2023-11-07 08:04:39 -0500

    Added parse-structured-data script.

--- src/encoded/commands/parse_structured_data.py
+++ src/encoded/commands/parse_structured_data.py
@@ -1,6 +1,6 @@
 import argparse
 import json
-import jsonschema
+from jsonschema import Draft7Validator as JsonSchemaValidator
 from typing import Optional
 from dcicutils.bundle_utils import load_items as sheet_utils_load_items
 from ..ingestion.structured_data import Portal, Schema, StructuredDataSet
@@ -10,7 +10,10 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Parse local structured data file for dev/testing purposes.")
     parser.add_argument("file", type=str, help=f"File to parse.")
     parser.add_argument("--new", required=False, action="store_true", default=False, help=f"Use new parser.")
-    parser.add_argument("--validate", required=False, action="store_true", default=False, help=f"Validation using JSON schema.")
+    parser.add_argument("--validate", required=False, action="store_true",
+                        default=False, help=f"Validation using JSON schema.")
+    parser.add_argument("--no-format-validate", required=False, action="store_true",
+                        default=False, help=f"Do not do format checking on JSON schema validation.")
     args = parser.parse_args()
 
     portal = Portal.create_for_testing()
@@ -21,13 +24,13 @@ def main() -> None:
     if args.validate:
         for data_type in parsed_data:
             data_of_type = parsed_data[data_type]
-            schema = Schema.load_by_name(data_type).data
-            schema_validator = jsonschema.Draft7Validator(schema)
+            schema = Schema.load_by_name(data_type, portal).data
+            schema_format_checker = JsonSchemaValidator.FORMAT_CHECKER if not args.no_format_validate else None
+            schema_validator = JsonSchemaValidator(schema, format_checker=schema_format_checker)
             for index, data in enumerate(data_of_type):
                 for validation_error in schema_validator.iter_errors(data):
                     print(f"VALIDATION ERROR: {data_type} [{index}]:")
                     print(validation_error)
-                    import pdb ; pdb.set_trace()
                 pass
             pass
 
