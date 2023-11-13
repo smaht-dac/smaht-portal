5d24beb4dac13b9d070c8151370e84f09ac1ddfe
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2023-11-13 10:57:28 -0500

    Nav tweaks for benchmarking

--- docs/public/benchmarking_COLO829.jsx
+++ docs/public/benchmarking_COLO829.jsx
@@ -1,3 +1,3 @@
-<BenchmarkingUI>
-    <COLO829Data schemas={schemas} session={session} facets={null}/>
+<BenchmarkingUI href={href}>
+    <COLO829Data schemas={schemas} session={session} facets={null} href={href} />
 </BenchmarkingUI>
--- src/encoded/static/components/static-pages/components/BenchmarkingUI.js
+++ src/encoded/static/components/static-pages/components/BenchmarkingUI.js
@@ -12,13 +12,15 @@ import {
 import { EmbeddedItemSearchTable } from '../../item-pages/components/EmbeddedItemSearchTable';
 
 export const BenchmarkingUI = (props) => {
-    const { children } = props;
+    const { children, href } = props;
+
+    console.log('benchmarkingUI href', href);
 
     // pass schemas and session to each child
     return (
         <div className="row">
             <div className="d-none d-lg-flex col-lg-2 border-right">
-                <BenchmarkingUINav />
+                <BenchmarkingUINav {...{ href }} />
             </div>
             <div className="col-12 col-lg-10 pl-2">{children}</div>
         </div>
@@ -26,26 +28,28 @@ export const BenchmarkingUI = (props) => {
 };
 
 const BenchmarkingUINav = (props) => {
+    const { href } = props;
+
     return (
         <div className="w-100 benchmarking-nav">
             <div>
                 <span className="text-small text-600">Cell Line Data</span>
                 <div>
-                    <BenchmarkingUINavCellLines />
+                    <BenchmarkingUINavCellLines {...{ href }} />
                 </div>
             </div>
             <hr />
             <div>
                 <span className="text-small text-600">Primary Tissue Data</span>
                 <div>
-                    <BenchmarkingUINavPrimaryTissue />
+                    <BenchmarkingUINavPrimaryTissue {...{ href }} />
                 </div>
             </div>
         </div>
     );
 };
 
-export const COLO829Data = ({ schemas, session, facets }) => {
+export const COLO829Data = ({ schemas, session, facets, href }) => {
     return (
         <div>
             <h2>COLO829 Cell Line Data</h2>
@@ -104,7 +108,7 @@ function ContextAwareToggle({ children, eventKey, callback }) {
         <div className="d-flex justify-content-between align-items-center">
             <button
                 type="button"
-                className="border-0 bg-transparent"
+                className="border-0 bg-transparent m-0 p-0"
                 onClick={decoratedOnClick}>
                 <div>
                     {children}
@@ -116,119 +120,139 @@ function ContextAwareToggle({ children, eventKey, callback }) {
 }
 
 const BenchmarkingUINavCellLines = (props) => {
+    const { href } = props;
     return (
         <BenchmarkingUINavWrapper defaultActiveKey={'1'}>
             <BenchmarkingUINavDrop
                 eventKey="1"
+                pageHref={href}
                 href="/data/benchmarking/COLO829"
                 title="COLO829">
                 <ul>
-                    <li>
-                        <a href="/data/benchmarking/COLO829#main">COLO829</a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/COLO829BL#main">
-                            COLO829BL
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/COLO829#110">1:10</a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/COLO829#150">1:50</a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/COLO829#1200">1:200</a>
-                    </li>
+                    <BenchmarkingUINavLink
+                        title="COLO829"
+                        pageHref={href}
+                        href="/data/benchmarking/COLO829#main"
+                    />
+                    <BenchmarkingUINavLink
+                        title="COLO829BL"
+                        pageHref={href}
+                        href="/data/benchmarking/COLO829#BL"
+                    />
+                    <BenchmarkingUINavLink
+                        title="1:10"
+                        pageHref={href}
+                        href="/data/benchmarking/COLO829#110"
+                    />
+                    <BenchmarkingUINavLink
+                        title="1:50"
+                        pageHref={href}
+                        href="/data/benchmarking/COLO829#150"
+                    />
+                    <BenchmarkingUINavLink
+                        title="1:200"
+                        pageHref={href}
+                        href="/data/benchmarking/COLO829#1200"
+                    />
                 </ul>
             </BenchmarkingUINavDrop>
-            <li>
-                <a href="/data/benchmarking/HapMap">HapMap</a>
-            </li>
-            <li>
-                <a href="/data/benchmarking/iPSC-fibroblasts">
-                    iPSc & Fibroblasts
-                </a>
-            </li>
+            <BenchmarkingUINavLink
+                title="HapMap"
+                pageHref={href}
+                href="/data/benchmarking/HapMap"
+            />
+            <BenchmarkingUINavLink
+                title="iPSc & Fibroblasts"
+                pageHref={href}
+                href="/data/benchmarking/iPSC-fibroblasts"
+            />
         </BenchmarkingUINavWrapper>
     );
 };
 
 const BenchmarkingUINavPrimaryTissue = (props) => {
+    const { href } = props;
     return (
         <BenchmarkingUINavWrapper defaultActiveKey={'0'}>
             <BenchmarkingUINavDrop
                 eventKey="1"
+                pageHref={href}
                 href="/data/benchmarking/brain"
                 title="Brain">
                 <ul>
-                    <li>
-                        <a href="/data/benchmarking/brain#frontal-lobe">
-                            Frontal Lobe
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/brain#cerebellum">
-                            Cerebellum
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/brain#hippocampus">
-                            Hippocampus
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/brain#temporal-lobe">
-                            Temporal Lobe
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/brain#dendate-gyrus">
-                            Dendate Gyrus
-                        </a>
-                    </li>
+                    <BenchmarkingUINavLink
+                        title="Frontal Lobe"
+                        pageHref={href}
+                        href="/data/benchmarking/brain#frontal-lobe"
+                    />
+                    <BenchmarkingUINavLink
+                        title="Cerebellum"
+                        pageHref={href}
+                        href="/data/benchmarking/brain#cerebellum"
+                    />
+                    <BenchmarkingUINavLink
+                        title="Hippocampus"
+                        pageHref={href}
+                        href="/data/benchmarking/brain#hippocampus"
+                    />
+                    <BenchmarkingUINavLink
+                        title="Temporal Lobe"
+                        pageHref={href}
+                        href="/data/benchmarking/brain#temporal-lobe"
+                    />
+                    <BenchmarkingUINavLink
+                        title="Dendate Gyrus"
+                        pageHref={href}
+                        href="/data/benchmarking/brain#dendate-gyrus"
+                    />
                 </ul>
             </BenchmarkingUINavDrop>
             <BenchmarkingUINavDrop
                 eventKey="2"
+                pageHref={href}
                 href="/data/benchmarking/skin"
                 title="Skin">
                 <ul>
-                    <li>
-                        <a href="/data/benchmarking/skin#sun-exposed">
-                            Sun Exposed
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/skin#non-sun-exposed">
-                            Non Sun Exposed
-                        </a>
-                    </li>
+                    <BenchmarkingUINavLink
+                        title="Sun Exposed"
+                        pageHref={href}
+                        href="/data/benchmarking/skin#sun-exposed"
+                    />
+                    <BenchmarkingUINavLink
+                        title="Non Sun Exposed"
+                        pageHref={href}
+                        href="/data/benchmarking/skin#non-sun-exposed"
+                    />
                 </ul>
             </BenchmarkingUINavDrop>
-            <li>
-                <a href="/data/benchmarking/liver">Liver</a>
-            </li>
+            <BenchmarkingUINavLink
+                title="Liver"
+                pageHref={href}
+                href="/data/benchmarking/liver"
+            />
             <BenchmarkingUINavDrop
                 eventKey="3"
+                pageHref={href}
                 href="/data/benchmarking/colon"
                 title="Colon">
                 <ul>
-                    <li>
-                        <a href="/data/benchmarking/colon#ascending">
-                            Ascending
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/data/benchmarking/colon#descending">
-                            Descending
-                        </a>
-                    </li>
+                    <BenchmarkingUINavLink
+                        title="Ascending"
+                        pageHref={href}
+                        href="/data/benchmarking/colon#ascending"
+                    />
+                    <BenchmarkingUINavLink
+                        pageHref={href}
+                        title="Descending"
+                        href="/data/benchmarking/colon#descending"
+                    />
                 </ul>
             </BenchmarkingUINavDrop>
-            <li>
-                <a href="/data/benchmarking/colon">Heart</a>
-            </li>
+            <BenchmarkingUINavLink
+                pageHref={href}
+                title="Heart"
+                href="/data/benchmarking/heart"
+            />
         </BenchmarkingUINavWrapper>
     );
 };
@@ -256,6 +280,27 @@ const BenchmarkingUINavDrop = (props) => {
             <Accordion.Collapse {...{ eventKey }}>
                 {children}
             </Accordion.Collapse>
+        </li>
+    );
+};
+
+const BenchmarkingUINavLink = (props) => {
+    const { href, pageHref, title } = props;
+
+    const isActive = href === pageHref;
+    const activeStyle = 'bg-primary';
+    const inactiveStyle = '';
+
+    // console.log("href", href);
+    // console.log("pageHref", pageHref);
+
+    return (
+        <li>
+            <a
+                {...{ href }}
+                className={`${isActive ? activeStyle : inactiveStyle}`}>
+                {title}
+            </a>
         </li>
     );
 };
