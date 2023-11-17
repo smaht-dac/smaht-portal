861921fd9209b24567370758c46a8a4d0bbcac98
David Michaels <david_michaels@hms.harvard.edu>
2023-11-17 08:07:13 -0500

    Starting some tests.

--- src/encoded/tests/test_structured_data.py
+++ src/encoded/tests/test_structured_data.py
@@ -8,56 +8,6 @@ from encoded.ingestion.ingestion_processors import parse_structured_data
 THIS_TEST_MODULE_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 TEST_FILES_DIR = f"{THIS_TEST_MODULE_DIRECTORY}/data/test_files"
 
-def _assert_parse_structured_data(file: str, rows: Optional[List[str]] = None,
-                                  expected: Union[dict, list] = None,
-                                  expected_validation_errors: Union[dict, list] = None,
-                                  debug: bool = False) -> None:
-    portal = Portal.create_for_unit_testing()
-    if rows:
-        if os.path.exists(file) or os.path.exists(os.path.join(TEST_FILES_DIR, file)):
-            raise Exception("Attempt to create temporary file with same name as existing test file: {file}")
-        with Utils.temporary_file(name=file, content=rows) as tmp_file_name:
-            structured_data, validation_errors = parse_structured_data(file=tmp_file_name, portal=portal)
-    else:
-        if os.path.exists(os.path.join(TEST_FILES_DIR, file)):
-            file = os.path.join(TEST_FILES_DIR, file)
-        elif not os.path.exists(file):
-            raise Exception(f"Cannot find input test file: {file}")
-        structured_data, validation_errors = parse_structured_data(file=file, portal=portal)
-    if debug:
-        import pdb
-        pdb.set_trace()
-    assert structured_data == expected
-    if expected_validation_errors:
-        assert validation_errors == expected_validation_errors
-
-
-def _test_parse_structured_data(file: str, rows: Optional[List[str]] = None,
-                                expected: Union[dict, list] = None,
-                                expected_validation_errors: Optional[Union[dict, list]] = None,
-                                noschemas: bool = False,
-                                norefs: bool = False,
-                                refs: Optional[List[str]] = None,
-                                debug: bool = False) -> None:
-    def assert_parse_structured_data():
-        _assert_parse_structured_data(file=file, rows=rows,
-                                      expected=expected,
-                                      expected_validation_errors=expected_validation_errors,
-                                      debug=debug)
-    if noschemas:
-        if norefs or refs:
-            with _mocked_schemas():
-                with _mocked_refs(refs):
-                    assert_parse_structured_data()
-        else:
-            with _mocked_schemas():
-                assert_parse_structured_data()
-    elif norefs or refs:
-        with _mocked_refs(refs):
-            assert_parse_structured_data()
-    else:
-        assert_parse_structured_data()
-
 
 def test_parse_structured_data_1():
     _test_parse_structured_data(file = "test.csv", noschemas = True, rows = [
@@ -84,6 +34,7 @@ def test_parse_structured_data_1():
         }
       ]
     })
+
 
 def test_parse_structured_data_2():
     _test_parse_structured_data(file = f"submission_test_file_from_doug_20231106.xlsx",
@@ -160,22 +111,73 @@ def test_parse_structured_data_2():
     )
 
 
-@contextmanager
-def _mocked_schemas():
-    def schema_load_by_name(name: str, portal: Portal) -> Optional[dict]:
-        return None
-    with mock.patch("encoded.ingestion.structured_data.Schema.load_by_name", side_effect=schema_load_by_name):
-        yield
+
+def _test_parse_structured_data(file: str, rows: Optional[List[str]] = None,
+                                expected: Union[dict, list] = None,
+                                expected_validation_errors: Optional[Union[dict, list]] = None,
+                                noschemas: bool = False,
+                                norefs: bool = False,
+                                refs: Optional[List[str]] = None,
+                                debug: bool = False) -> None:
+
+    def call_parse_structured_data(file: str, rows: Optional[List[str]] = None,
+                                      expected: Union[dict, list] = None,
+                                      expected_validation_errors: Union[dict, list] = None,
+                                      debug: bool = False) -> None:
+        portal = Portal.create_for_unit_testing()
+        if rows:
+            if os.path.exists(file) or os.path.exists(os.path.join(TEST_FILES_DIR, file)):
+                raise Exception("Attempt to create temporary file with same name as existing test file: {file}")
+            with Utils.temporary_file(name=file, content=rows) as tmp_file_name:
+                structured_data, validation_errors = parse_structured_data(file=tmp_file_name, portal=portal)
+        else:
+            if os.path.exists(os.path.join(TEST_FILES_DIR, file)):
+                file = os.path.join(TEST_FILES_DIR, file)
+            elif not os.path.exists(file):
+                raise Exception(f"Cannot find input test file: {file}")
+            structured_data, validation_errors = parse_structured_data(file=file, portal=portal)
+        if debug:
+            import pdb
+            pdb.set_trace()
+        assert structured_data == expected
+        if expected_validation_errors:
+            assert validation_errors == expected_validation_errors
+
+    def assert_parse_structured_data():
+        call_parse_structured_data(file=file, rows=rows,
+                                   expected=expected,
+                                   expected_validation_errors=expected_validation_errors,
+                                   debug=debug)
+
+    @contextmanager
+    def mocked_schemas():
+        def schema_load_by_name(name: str, portal: Portal) -> Optional[dict]:
+            return None
+        with mock.patch("encoded.ingestion.structured_data.Schema.load_by_name", side_effect=schema_load_by_name):
+            yield
 
 
-@contextmanager
-def _mocked_refs(refs: Optional[List[str]] = None):
-    mocked_found_result = {"dummy": True}
+    @contextmanager
+    def mocked_refs(refs: Optional[List[str]] = None):
+        mocked_found_result = {"dummy": True}
+        def schema_get_metadata(object_name: str) -> Optional[dict]:
+            nonlocal refs
+            if refs:
+                return mocked_found_result if object_name in refs else None
+            return mocked_found_result
+        with mock.patch("encoded.ingestion.structured_data.Portal.get_metadata", side_effect=schema_get_metadata):
+            yield
 
-    def schema_get_metadata(object_name: str) -> Optional[dict]:
-        nonlocal refs
-        if refs:
-            return mocked_found_result if object_name in refs else None
-        return mocked_found_result
-    with mock.patch("encoded.ingestion.structured_data.Portal.get_metadata", side_effect=schema_get_metadata):
-        yield
+    if noschemas:
+        if norefs or refs:
+            with mocked_schemas():
+                with mocked_refs(refs):
+                    assert_parse_structured_data()
+        else:
+            with mocked_schemas():
+                assert_parse_structured_data()
+    elif norefs or refs:
+        with mocked_refs(refs):
+            assert_parse_structured_data()
+    else:
+        assert_parse_structured_data()
