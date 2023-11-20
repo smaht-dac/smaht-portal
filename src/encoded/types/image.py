1dfe999e9b3d027ef91d0782c78cd1126d657176
William Ronchetti <wrr33@cornell.edu>
2023-11-20 16:39:02 -0500

    fix test

--- src/encoded/types/image.py
+++ src/encoded/types/image.py
@@ -1,12 +1,13 @@
 from snovault import collection, load_schema
 from encoded_core.types.image import Image as CoreImage
-
+from .acl import ALLOW_SUBMISSION_CENTER_CREATE_ACL
 from .base import Item as SMAHTItem
 
 
 @collection(
     name='images',
     unique_key='image:filename',
+    acl=ALLOW_SUBMISSION_CENTER_CREATE_ACL,
     properties={
         'title': 'Image',
         'description': 'Listing of portal images',
