06a5246107975fe4462e4c872a5b820d48d26892
William Ronchetti <wrr33@cornell.edu>
2023-11-20 16:05:47 -0500

    fix statuses, ingester hard code

--- src/encoded/types/base.py
+++ src/encoded/types/base.py
@@ -254,7 +254,7 @@ def validate_user_submission_consistency(context, request):
         part of
     """
     user = request.environ.get('REMOTE_USER')
-    if not user or user in ['TEST', 'INDEXER', 'EMBED', 'TEST_SUBMITTER']:
+    if not user or user in ['TEST', 'INDEXER', 'EMBED', 'TEST_SUBMITTER', 'INGESTER']:
         return
     user = request.embed(f'/users/{user}?frame=raw')
     if 'group.admin' in user.get('groups', []):
