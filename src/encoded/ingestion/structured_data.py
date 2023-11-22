ab98ae6f5cb29907fcd6e181cd6771367d828f44
David Michaels <david_michaels@hms.harvard.edu>
2023-11-22 11:00:07 -0500

    Factored on Excel/CSV readers to dcicutils.

--- src/encoded/ingestion/structured_data.py
+++ src/encoded/ingestion/structured_data.py
@@ -1,17 +1,15 @@
-import abc
 import copy
-import csv
 from functools import lru_cache
 import json
 from jsonschema import Draft7Validator as SchemaValidator
-import openpyxl
 import os
 import re
 import sys
-from typing import Any, Callable, Generator, Iterator, List, Optional, Tuple, Type, Union
+from typing import Any, Callable, List, Optional, Tuple, Type, Union
 from webtest.app import TestApp
+from dcicutils.data_readers import CsvReader, Excel, RowReader
 from dcicutils.ff_utils import get_metadata, get_schema
-from dcicutils.misc_utils import (merge_objects, remove_empty_properties, right_trim, split_string,
+from dcicutils.misc_utils import (load_json_if, merge_objects, remove_empty_properties, split_string,
                                   to_boolean, to_camel_case, to_enum, to_float, to_integer, VirtualApp)
 from dcicutils.zip_utils import temporary_file, unpack_gz_file_to_temporary_file, unpack_files
 from snovault.loadxl import create_testapp
@@ -36,7 +34,6 @@ EXPERIMENTAL = True
 # Forward type references for type hints.
 Portal = Type["Portal"]
 PortalAny = Union[VirtualApp, TestApp, Portal]
-RowReader = Type["RowReader"]
 Schema = Type["Schema"]
 StructuredDataSet = Type["StructuredDataSet"]
 
@@ -244,7 +241,7 @@ class Schema:
         if (map_value := self._typeinfo.get(column_name, {}).get("map")) is None:
             map_value = self._typeinfo.get(column_name + ARRAY_NAME_SUFFIX_CHAR, {}).get("map")
         src = f"{self.name}{f'.{column_name}' if column_name else ''}{f' [{loc}]' if loc else ''}"
-        return map_value(value, src) if map_value else _load_json_if(value, is_object=True, is_array=True)
+        return map_value(value, src) if map_value else load_json_if(value, is_object=True, is_array=True)
         
     def _map_function(self, typeinfo: dict) -> Optional[Callable]:
         MAP_FUNCTIONS = {
@@ -277,7 +274,7 @@ class Schema:
 
     def _map_function_array(self, typeinfo: dict) -> Callable:
         def map_array(value: str, array_type_map_function: Optional[Callable], src: Optional[str]) -> Any:
-            value = _split_array_string(value) if array_type_map_function else _load_json_if(value, is_array=True)
+            value = _split_array_string(value) if array_type_map_function else load_json_if(value, is_array=True)
             return [array_type_map_function(value, src) for value in value] if array_type_map_function else value
         return lambda value, src: map_array(value, self._map_function(typeinfo), src)
 
@@ -398,139 +395,6 @@ class Schema:
                                                 for value in _split_dotted_string(column_name)])
 
 
-class RowReader(abc.ABC):  # These readers may evenutally go into dcicutils.
-
-    def __init__(self):
-        self.header = None
-        self.location = 0
-        self._warning_empty_headers = False
-        self._warning_extra_values = []  # Line numbers.
-        self.open()
-
-    def __iter__(self) -> Iterator:
-        for row in self.rows:
-            self.location += 1
-            if self.is_terminating_row(row):
-                break
-            if len(self.header) < len(row): # Row values beyond what there are headers for are ignored.
-                self._warning_extra_values.append(self.location)
-            yield {column: self.cell_value(value) for column, value in zip(self.header, row)}
-
-    def _define_header(self, header: List[Optional[Any]]) -> None:
-        self.header = []
-        for index, column in enumerate(header or []):
-            if not (column := str(column).strip() if column is not None else ""):
-                self._warning_empty_headers = True
-                break  # Empty header column signals end of header.
-            self.header.append(column)
-
-    def rows(self) -> Generator[Union[List[Optional[Any]], Tuple[Optional[Any], ...]], None, None]:
-        yield
-
-    def is_terminating_row(self, row: Union[List[Optional[Any]], Tuple[Optional[Any]]]) -> bool:
-        return False
-
-    def cell_value(self, value: Optional[Any]) -> Optional[Any]:
-        return str(value).strip() if value is not None else ""
-
-    def open(self) -> None:
-        pass
-
-    @property
-    def issues(self) -> Optional[List[str]]:
-        issues = []
-        if self._warning_empty_headers:
-            issues.append("Empty header column encountered; ignoring it and all subsequent columns.")
-        if self._warning_extra_values:
-            issues.extend([f"Extra column values on row [{row_number}]" for row_number in self._warning_extra_values])
-        return issues if issues else None
-
-
-class ListReader(RowReader):
-
-    def __init__(self, rows: List[List[Optional[Any]]]) -> None:
-        self._rows = rows
-        super().__init__()
-
-    @property
-    def rows(self) -> Generator[List[Optional[Any]], None, None]:
-        for row in self._rows[1:]:
-            yield row
-
-    def open(self) -> None:
-        if not self.header:
-            self._define_header(self._rows[0] if self._rows else [])
-
-
-class CsvReader(RowReader):
-
-    def __init__(self, file: str) -> None:
-        self._file = file
-        self._file_handle = None
-        self._rows = None
-        super().__init__()
-
-    @property
-    def rows(self) -> Generator[List[Optional[Any]], None, None]:
-        for row in self._rows:
-            yield right_trim(row)
-
-    def open(self) -> None:
-        if self._file_handle is None:
-            self._file_handle = open(self._file)
-            self._rows = csv.reader(self._file_handle, delimiter="\t" if self._file.endswith(".tsv") else ",")
-            self._define_header(right_trim(next(self._rows, [])))
-
-    def __del__(self) -> None:
-        if (file_handle := self._file_handle) is not None:
-            self._file_handle = None
-            file_handle.close()
-
-
-class ExcelSheetReader(RowReader):
-
-    def __init__(self, sheet_name: str, workbook: openpyxl.workbook.workbook.Workbook) -> None:
-        self.sheet_name = sheet_name or "Sheet1"
-        self._workbook = workbook
-        self._rows = None
-        super().__init__()
-
-    @property
-    def rows(self) -> Generator[Tuple[Optional[Any], ...], None, None]:
-        for row in self._rows(min_row=2, values_only=True):
-            yield right_trim(row)
-
-    def is_terminating_row(self, row: Tuple[Optional[Any]]) -> bool:
-        return all(cell is None for cell in row)  # Empty row signals end of data.
-
-    def open(self) -> None:
-        if not self._rows:
-            self._rows = self._workbook[self.sheet_name].iter_rows
-            self._define_header(right_trim(next(self._rows(min_row=1, max_row=1, values_only=True), [])))
-
-
-class Excel:
-
-    def __init__(self, file: str) -> None:
-        self._file = file
-        self._workbook = None
-        self.sheet_names = None
-        self.open()
-
-    def sheet_reader(self, sheet_name: str) -> ExcelSheetReader:
-        return ExcelSheetReader(sheet_name=sheet_name, workbook=self._workbook)
-
-    def open(self) -> None:
-        if self._workbook is None:
-            self._workbook = openpyxl.load_workbook(self._file, data_only=True)
-            self.sheet_names = self._workbook.sheetnames or []
-
-    def __del__(self) -> None:
-        if (workbook := self._workbook) is not None:
-            self._workbook = None
-            workbook.close()
-
-
 class Portal:
 
     def __init__(self, portal: PortalAny, data: Optional[dict] = None, schemas: Optional[List[dict]] = None) -> None:
@@ -591,13 +455,3 @@ def _split_dotted_string(value: str):
 
 def _split_array_string(value: str):
     return split_string(value, ARRAY_VALUE_DELIMITER_CHAR, ARRAY_VALUE_DELIMITER_ESCAPE_CHAR)
-
-
-def _load_json_if(value: str, is_array: bool = False, is_object: bool = False) -> Optional[Any]:
-    if ((is_object and value.startswith("{") and value.endswith("}")) or
-        (is_array and value.startswith("[") and value.endswith("]"))) and EXPERIMENTAL:
-        try:  # A sort of back door to allow specifying actual JSON for values if no mapping.
-            return json.loads(value)
-        except Exception:
-            pass
-    return value
