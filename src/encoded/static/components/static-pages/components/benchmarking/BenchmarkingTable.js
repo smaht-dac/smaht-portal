f83d6e81f68b48bafa99d281618c020df319b228
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2023-12-08 11:31:28 -0500

    Start general workflow for tsv download button

--- src/encoded/static/components/static-pages/components/benchmarking/BenchmarkingTable.js
+++ src/encoded/static/components/static-pages/components/benchmarking/BenchmarkingTable.js
@@ -1,15 +1,20 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useMemo } from 'react';
 import PropTypes from 'prop-types';
 import queryString from 'query-string';
 import _ from 'underscore';
+import { Modal } from 'react-bootstrap';
 
 import {
     ajax,
-    // analytics,
+    analytics,
     memoizedUrlParse,
+    object,
     logger,
+    JWT,
 } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
+import { display as dateTimeDisplay } from '@hms-dbmi-bgm/shared-portal-components/es/components/ui/LocalizedTime';
 import { SelectedItemsController } from '@hms-dbmi-bgm/shared-portal-components/es/components/browse/components/SelectedItemsController';
+
 import { EmbeddedItemSearchTable } from '../../../item-pages/components/EmbeddedItemSearchTable';
 
 export const BenchmarkingTableController = (props) => {
@@ -59,6 +64,7 @@ const BenchmarkingTable = (props) => {
             aboveTableComponent={
                 <BenchmarkingAboveTableComponent
                     {...{
+                        session,
                         selectedItems,
                         onSelectItem,
                         onResetSelectedItems,
@@ -67,7 +73,7 @@ const BenchmarkingTable = (props) => {
                     }}
                 />
             }
-            rowHeight={40}
+            rowHeight={31}
             // maxHeight={200}
             {...{
                 searchHref,
@@ -96,6 +102,7 @@ const BenchmarkingAboveTableComponent = React.memo(
             addHiddenColumn,
             removeHiddenColumn,
             columnDefinitions,
+            session,
             selectedItems, // From SelectedItemsController
             onSelectItem, // From SelectedItemsController
             onResetSelectedItems, // From SelectedItemsController
@@ -126,13 +133,14 @@ const BenchmarkingAboveTableComponent = React.memo(
                         {...{ searchHref, href, context }}
                         totalFilesCount={totalResultCount}
                     />
-                    <button
-                        type="button"
+                    <SelectedItemsDownloadButton
+                        id="download_tsv_multiselect"
                         disabled={selectedItems.size === 0}
-                        className="btn btn-primary btn-sm mr-05 align-items-center">
+                        className="btn btn-primary btn-sm mr-05 align-items-center"
+                        {...{ selectedItems, session }}>
                         <i className="icon icon-download fas mr-03" />
                         Download {selectedItems.size} Selected Files
-                    </button>
+                    </SelectedItemsDownloadButton>
                 </div>
             </div>
         );
@@ -295,3 +303,297 @@ class SelectAllFilesButton extends React.PureComponent {
         );
     }
 }
+
+/**
+ * Upon clicking the button, reveal a modal popup giving users more download instructions.
+ */
+export class SelectedItemsDownloadButton extends React.PureComponent {
+    static propTypes = {
+        id: PropTypes.string,
+        selectedItems: PropTypes.object.isRequired,
+        filenamePrefix: PropTypes.string.isRequired,
+        children: PropTypes.node.isRequired,
+        disabled: PropTypes.bool,
+        context: PropTypes.object,
+        session: PropTypes.bool,
+        action: PropTypes.string,
+        // analyticsAddFilesToCart: PropTypes.bool,
+    };
+
+    static defaultProps = {
+        id: null,
+        filenamePrefix: 'smaht_metadata_',
+        children: 'Download',
+        className: 'btn-primary',
+        // analyticsAddFilesToCart: false,
+        action: '/metadata/?type=ExperimentSet&sort=accession',
+    };
+
+    constructor(props) {
+        super(props);
+        _.bindAll(this, 'hideModal', 'showModal');
+        this.state = { modalOpen: false };
+    }
+
+    hideModal() {
+        this.setState({ modalOpen: false });
+    }
+
+    showModal() {
+        this.setState({ modalOpen: true });
+    }
+
+    render() {
+        const {
+            selectedItems,
+            filenamePrefix,
+            children,
+            disabled,
+            // analyticsAddFilesToCart,
+            action,
+            session,
+            ...btnProps
+        } = this.props;
+        const { modalOpen } = this.state;
+        const isDisabled =
+            typeof disabled === 'boolean' ? disabled : fileCountWithDupes === 0;
+        btnProps.className =
+            'btn ' + (modalOpen ? 'active ' : '') + btnProps.className;
+        return (
+            <React.Fragment>
+                <button
+                    type="button"
+                    {...btnProps}
+                    disabled={isDisabled}
+                    onClick={this.showModal}>
+                    {children}
+                </button>
+                {modalOpen ? (
+                    <SelectedItemsDownloadModal
+                        {...{
+                            selectedItems,
+                            filenamePrefix,
+                            // analyticsAddFilesToCart,
+                            action,
+                            session,
+                        }}
+                        onHide={this.hideModal}
+                    />
+                ) : null}
+            </React.Fragment>
+        );
+    }
+}
+
+const SelectedItemsDownloadModal = function (props) {
+    const { onHide, filenamePrefix, selectedItems, session } = props;
+    let { action } = props;
+
+    const suggestedFilename =
+        filenamePrefix +
+        dateTimeDisplay(new Date(), 'date-time-file', '-', false) +
+        '.tsv';
+    const userInfo = JWT.getUserInfo();
+    const profileHref =
+        (session &&
+            userInfo.user_actions &&
+            _.findWhere(userInfo.user_actions, { id: 'profile' }).href) ||
+        '/me';
+
+    if ('search' === analytics.hrefToListName(window && window.location.href)) {
+        action = '/metadata/?type=File&sort=accession';
+        // workaround for file only search since type=File returns more than 10K results that prevent iterating all after ES7 upgrade
+        const parts = _.clone(memoizedUrlParse(window.location.href));
+        const modifiedQuery = _.omit(parts.query || {}, [
+            'currentAction',
+            'type',
+            'sort',
+            'limit',
+        ]);
+        const modifiedSearch = queryString.stringify(modifiedQuery);
+        action += '&' + modifiedSearch;
+    }
+
+    return (
+        <Modal
+            show
+            className="batch-files-download-modal"
+            onHide={onHide}
+            bsSize="large">
+            <Modal.Header closeButton>
+                <Modal.Title>
+                    <span className="text-400">
+                        Download{' '}
+                        <span className="text-600">{selectedItems.length}</span>{' '}
+                        Files
+                    </span>
+                </Modal.Title>
+            </Modal.Header>
+
+            <Modal.Body>
+                <div className="important-notes-section">
+                    <h4 className="mb-07 text-500">Important</h4>
+                    <ul>
+                        <li className="mb-05">
+                            <span className="text-danger">
+                                You must include an <b>access key</b> in your
+                                cURL command for bulk downloads.
+                            </span>
+                        </li>
+                        <li className="mb-05">
+                            You can configure the access key in{' '}
+                            {session ? (
+                                <a
+                                    href={profileHref}
+                                    target="_blank"
+                                    rel="noopener noreferrer">
+                                    your profile
+                                </a>
+                            ) : (
+                                'your profile'
+                            )}
+                            , then use it in place of{' '}
+                            <em>{'<access_key_id>:<access_key_secret>'}</em>,
+                            below.
+                        </li>
+                        {!session ? (
+                            <li>
+                                {
+                                    "If you don't already have an account, you can "
+                                }
+                                <a
+                                    onClick={onLoginNavItemClick}
+                                    href="#loginbtn">
+                                    log in
+                                </a>
+                                {' with your Google or GitHub credentials.'}
+                            </li>
+                        ) : null}
+                    </ul>
+                </div>
+                <p>
+                    Please press the &quot;Download&quot; button below to save
+                    the metadata TSV file which contains download URLs and other
+                    information for the selected files.
+                </p>
+                <p>
+                    Once you have saved the metadata TSV, you may download the
+                    files on any machine or server with the following cURL
+                    command:
+                </p>
+                <ModalCodeSnippet
+                    filename={suggestedFilename}
+                    session={session}
+                />
+
+                <SelectedItemsDownloadStartButton
+                    {...{
+                        selectedItems,
+                        suggestedFilename,
+                        action,
+                    }}
+                />
+
+                <button
+                    type="reset"
+                    onClick={onHide}
+                    className="btn btn-outline-dark mt-1 btn-block-xs-only">
+                    Cancel
+                </button>
+            </Modal.Body>
+        </Modal>
+    );
+};
+
+const ModalCodeSnippet = React.memo(function ModalCodeSnippet(props) {
+    const { filename, session } = props;
+    const htmlValue = (
+        <pre className="mb-15 d-md-inline curl-command">
+            cut -f 1 <b>{filename}</b> | tail -n +3 | grep -v ^# | xargs -n 1
+            curl -O -L
+            {session ? (
+                <code style={{ opacity: 0.5 }}>
+                    {' '}
+                    --user <em>{'<access_key_id>:<access_key_secret>'}</em>
+                </code>
+            ) : null}
+        </pre>
+    );
+    const plainValue =
+        `cut -f 1 ${filename} | tail -n +3 | grep -v ^# | xargs -n 1 curl -O -L` +
+        (session ? ' --user <access_key_id>:<access_key_secret>' : '');
+    return (
+        <object.CopyWrapper
+            value={plainValue}
+            className="curl-command-wrapper"
+            data-tip={'Click to copy'}
+            wrapperElement="div"
+            iconProps={{}}>
+            {htmlValue}
+        </object.CopyWrapper>
+    );
+});
+
+/**
+ * Use this button to download the tsv file metadata.
+ *
+ * Renders out a literal form (gets caught by App.js and serialized to JSON),
+ * with 'accessions' (String[]) included in the POSTed form fields which identify the individual files to download.
+ */
+const SelectedItemsDownloadStartButton = React.memo(
+    function SelectedItemsDownloadStartButton(props) {
+        const { suggestedFilename, selectedItems, action } = props;
+
+        const { accessionArray } = useMemo(
+            function () {
+                // Flatten selected items into an array (currently a set, I believe)
+                console.log('selectedItems', selectedItems);
+
+                const itemAtIds = Array.from(selectedItems.keys());
+                console.log('itemAtIds', itemAtIds);
+
+                const accessionArray = [];
+
+                itemAtIds.forEach((atId) => {
+                    const value = selectedItems.get(atId);
+
+                    if (value?.accession) {
+                        accessionArray.push(value.accession);
+                    } else {
+                        throw new Error('File Item without accession found!');
+                    }
+                });
+
+                return { accessionArray };
+            },
+            [selectedItems]
+        );
+
+        return (
+            <form
+                method="POST"
+                action={action}
+                className="d-inline-block d-block-xs-only">
+                <input
+                    type="hidden"
+                    name="accessions"
+                    value={JSON.stringify(accessionArray)}
+                />
+                <input
+                    type="hidden"
+                    name="download_file_name"
+                    value={JSON.stringify(suggestedFilename)}
+                />
+                <button
+                    type="submit"
+                    name="Download"
+                    className="btn btn-primary mt-1 mr-1 btn-block-xs-only"
+                    // onClick={onClick} // TODO: re-add onclick to handle analytics move-to-cart
+                    data-tip="Details for each individual selected file delivered via a TSV spreadsheet.">
+                    <i className="icon icon-fw icon-file-alt fas mr-1" />
+                    Download metadata for files
+                </button>
+            </form>
+        );
+    }
+);
