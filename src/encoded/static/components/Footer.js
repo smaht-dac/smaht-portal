a03b1aa4b26b3472f218edea1b00576c6ef36364
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2023-12-11 13:20:28 -0500

    style: move footer left

--- src/encoded/static/components/Footer.js
+++ src/encoded/static/components/Footer.js
@@ -12,9 +12,9 @@ import PropTypes from 'prop-types';
 export const Footer = React.memo(function Footer() {
     return (
         <footer id="page-footer">
-            <div className="page-footer container">
+            <div className="page-footer container-full">
                 <div className="row">
-                    <div className="col-12 col-md-6">
+                    <div className="col-12 col-md-6 px-sm-4">
                         <div className="footer-section copy-notice d-flex align-items-center justify-content-center justify-content-sm-start">
                             <div>
                                 SMaHT is funded by the{' '}
