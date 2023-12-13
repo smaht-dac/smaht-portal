cc318305885d13fae7a5c3e6bae66f4e8eb25aba
David Michaels <david_michaels@hms.harvard.edu>
2023-12-13 10:21:40 -0500

    remove-pdb

--- src/encoded/tests/test_structured_data.py
+++ src/encoded/tests/test_structured_data.py
@@ -1251,7 +1251,7 @@ def _test_parse_structured_data(testapp: TestApp,
         def call_parse_structured_data(file: str):
             nonlocal portal, novalidate, debug
             if debug:
-                import pdb ; pdb.set_trace()
+                # import pdb ; pdb.set_trace()
                 pass
             return parse_structured_data(file=file, portal=portal, novalidate=novalidate,
                                          prune=True if prune is not False else False)
@@ -1281,11 +1281,11 @@ def _test_parse_structured_data(testapp: TestApp,
                 structured_data = structured_data_set.data
                 validation_errors = structured_data_set.validation_errors
         if debug:
-            import pdb ; pdb.set_trace()
+            # import pdb ; pdb.set_trace()
             pass
         if expected is not None:
             if not (structured_data == expected):
-                import pdb ; pdb.set_trace()
+                # import pdb ; pdb.set_trace()
                 pass
             assert structured_data == expected
         if expected_errors:
@@ -1374,7 +1374,7 @@ def _get_schema_flat_typeinfo(schema: Schema):
 
 def _test_structured_row_data(columns: str, expected: Optional[dict]):
     if _StructuredRowTemplate(columns.split(","))._template != expected:
-        import pdb ; pdb.set_trace()
+        # import pdb ; pdb.set_trace()
         pass
     assert _StructuredRowTemplate(columns.split(","))._template == expected
 
