3e3069aeb2ca27e8253df32a1139da64f3604320
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-01-02 15:25:15 -0500

    Fix tests

--- src/encoded/tests/test_types_aligned_reads.py
+++ src/encoded/tests/test_types_aligned_reads.py
@@ -9,4 +9,4 @@ def test_submitted_id_resource_path(es_testapp: TestApp, workbook: None) -> None
     """Ensure submitted_id is resource path for aligned reads file
     within SubmittedFile collection.
     """
-    get_item(es_testapp, "TEST_FILE_BAM", collection="SubmittedFile", status=301)
+    get_item(es_testapp, "TEST_FILE_SOME_BAM", collection="SubmittedFile", status=301)
--- src/encoded/tests/test_types_variant_calls.py
+++ src/encoded/tests/test_types_variant_calls.py
@@ -9,4 +9,4 @@ def test_submitted_id_resource_path(es_testapp: TestApp, workbook: None) -> None
     """Ensure submitted_id is resource path for variant calls file
     within SubmittedFile collection.
     """
-    get_item(es_testapp, "TEST_FILE_VCF", collection="SubmittedFile", status=301)
+    get_item(es_testapp, "TEST_FILE_SOME_VCF", collection="SubmittedFile", status=301)
