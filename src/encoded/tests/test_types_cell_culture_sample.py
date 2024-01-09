39172d9ecf44558afde0ddb07090c293bd7b9485
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-01-09 15:26:29 -0500

    Update tests for workbook changes

--- src/encoded/tests/test_types_cell_culture_sample.py
+++ src/encoded/tests/test_types_cell_culture_sample.py
@@ -9,4 +9,6 @@ def test_submitted_id_resource_path(es_testapp: TestApp, workbook: None) -> None
     """Ensure submitted_id is resource path for cell culture sample
     within Sample collection.
     """
-    get_item(es_testapp, "TEST_SAMPLE_HELA", collection="Sample", status=301)
+    get_item(
+        es_testapp, "TEST_CELL-CULTURE-SAMPLE_HELA", collection="Sample", status=301
+    )
