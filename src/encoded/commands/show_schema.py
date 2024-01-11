2acdc39516fe213f1f9ce6bd93a2476544baec85
David Michaels <david_michaels@hms.harvard.edu>
2024-01-10 19:06:29 -0500

    Test fixup to insulate from schema changes.

--- src/encoded/commands/show_schema.py
+++ src/encoded/commands/show_schema.py
@@ -12,7 +12,7 @@ def main() -> None:
     parser = argparse.ArgumentParser(description="Show schemas for dev/testing purposes.")
     parser.add_argument("schema", type=str, help=f"Schema to show.")
     parser.add_argument("--env", type=str, required=False, default=None,
-                        help=f"Environment name (key from ~/.smaht-keys.json).")
+                        help=f"Environment name (e.g. via ~/.smaht-keys.json).")
     parser.add_argument("--raw", required=False, action="store_true",
                         default=False, help=f"Raw JSON output with no formating.") 
     parser.add_argument("--yaml", required=False, action="store_true",
@@ -24,19 +24,23 @@ def main() -> None:
     with captured_output():
         portal = Portal(args.env) if args.env else Portal.create_for_testing()
 
-    schema = Schema.load_by_name(args.schema, portal)
-    if not schema:
-        if schema_name := rummage_for_schema_name(portal, args.schema):
-            schema = Schema.load_by_name(schema_name, portal)
-        if not schema:
-            print(f"Schema not found: {args.schema}")
-            exit(1)
-    if args.yaml:
-        print(yaml.dump(schema.data))
-    elif args.raw:
-        print(json.dumps(schema.data, default=str))
+    if args.schema.lower() == "all":
+        schema = portal.get_schemas()
     else:
-        print(json.dumps(schema.data, indent=4, default=str))
+        schema = Schema.load_by_name(args.schema, portal)
+        if not schema:
+            if schema_name := rummage_for_schema_name(portal, args.schema):
+                schema = Schema.load_by_name(schema_name, portal)
+            if not schema:
+                print(f"Schema not found: {args.schema}")
+                exit(1)
+        schema = schema.data
+    if args.yaml:
+        print(yaml.dump(schema))
+    elif args.raw:
+        print(json.dumps(schema, default=str))
+    else:
+        print(json.dumps(schema, indent=4, default=str))
 
 
 def rummage_for_schema_name(portal: Portal, schema_name: str) -> Optional[str]:
