15f793f41f1d901c846faed18463dead72dd2628
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2024-01-22 13:20:50 -0500

    feat: add text to awardees and data pages

--- docs/public/alluvial.jsx
+++ docs/public/alluvial.jsx
@@ -1,1 +1,29 @@
-<Alluvial />
+<>
+    <h3>COLO829</h3>
+    <p>COLO829 (COLO829T) is a metastatic melanoma cancer cell line, which has a matched normal lymphoblast cell line, COLO892BL, derived from the same individual. For benchmarking analysis, COLO829T cells were mixed with COLO829BL cells at a mixture ratio of 1:50 (COLO829BLT50)”.</p>
+
+    <h3>Benchmarking study using the HapMap Cell Line Mixture</h3>
+    <p>For benchmarking analysis, two HapMap mixtures were created as the following:</p>
+    <p>Mixture 1 with six HapMap cell lines</p>
+    <ul>
+        <li>10% HG02622 (F, AFR)</li>
+        <li>2% for each HG002 (M, EUR/ASH JEW), HG02257 (F, AFR), and HG02486 (M, AFR)</li>
+        <li>0.5% HG00438 (F, EAS)</li>
+        <li>83.5% HG005 (M, CHB)</li>
+    </ul>
+
+    <p>Mixture 2 with three HapMap cell lines</p>
+    <ul>
+        <li>93% HG001 (F, EUR)</li>
+        <li>2% HG002 (M, EUR/ASH JEW)</li>
+        <li>5% HG005 (M, CHB)</li>
+    </ul>
+
+    <h3>Benchmarking study using iPSCs / Fibroblasts</h3>
+    Five induced pluripotent stem cell lines were derived from distinct skin fibroblast cell lines from five adults. 
+
+    <h3>Benchmarking study using primary tissues</h3>
+    Primary tissues were obtained from post-mortem donors.
+
+    <Alluvial />
+</>
--- docs/public/consortium_map.jsx
+++ docs/public/consortium_map.jsx
@@ -1,1 +1,19 @@
-<ConsortiumMap />
+<>
+    <p>The SMaHT (Somatic Mosaicism across Human Tissues) Network is a new NIH initiative to identify non-inherited, somatic variations in human genomes over time and to understand the role of such genetic variation in human health, development, aging, and disease.</p>
+
+    <p>The Network consists of:</p>
+    <ol>
+        <li>A Tissue Procurement Center that procures tissue samples from post-mortem donors</li>
+        <li>Five Genome Characterization Centers that generate sequencing data</li>
+        <li>A Data Analysis Center that uniformly processes the SMaHT data</li>
+        <li>14 Tools and Technology Development Projects what focus on developing novel experimental assays and computational methods to detect somatic mutations.</li>
+    </ol>
+
+    <p>The SMaHT Network aims to generate sequencing data using multiple short- and long-read technologies and profiling assays. Please refer to our data overview here for more information.</p>
+
+    <p>The location and more information about the SMaHT Network members are below.</p>
+
+    <p>For more information about the Network, please visit the SMaHT Organization Center website.</p>
+
+    <ConsortiumMap />
+</>
