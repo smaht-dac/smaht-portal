03776b362ee26aa688c2d65b536b0dc8b775ade2
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-01-23 16:11:23 -0500

    Improve display title for Software

--- src/encoded/types/software.py
+++ src/encoded/types/software.py
@@ -1,22 +1,53 @@
 from typing import Any, Dict, Optional, List
 
-from snovault import collection, load_schema, Item as SnovaultItem
 from encoded_core.types.software import Software as CoreSoftware
+from pyramid.request import Request
+from snovault import (
+    collection,
+    calculated_property,
+    display_title_schema,
+    load_schema,
+    Item as SnovaultItem,
+)
 
 from .base import Item
 
 
 @collection(
-    name='software',
-    unique_key='submitted_id',
+    name="software",
+    unique_key="submitted_id",
     properties={
-        'title': 'Software',
-        'description': 'Listing of software for analyses',
-    })
+        "title": "Software",
+        "description": "Listing of software for analyses",
+    },
+)
 class Software(Item, CoreSoftware):
-    item_type = 'software'
+    item_type = "software"
     schema = load_schema("encoded:schemas/software.json")
     embedded_list = []
 
-    def _update(self, properties: Dict[str, Any], sheets: Optional[List] = None) -> None:
+    def _update(
+        self, properties: Dict[str, Any], sheets: Optional[List] = None
+    ) -> None:
         return SnovaultItem._update(self, properties, sheets)
+
+    @calculated_property(schema=display_title_schema)
+    def display_title(
+        self,
+        request: Request,
+        code: Optional[str],
+        title: Optional[str],
+        name: Optional[str],
+        submitted_id: Optional[str],
+        accession: Optional[str],
+    ) -> str:
+        if code:
+            return code
+        if title:
+            return title
+        if name:
+            return name
+        if submitted_id:
+            return submitted_id
+        if accession:
+            return accession
