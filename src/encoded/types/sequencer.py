f2cf62a36193de9a9dced7847633740351776d47
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-01-23 16:06:12 -0500

    Improve Sequencer display title

--- src/encoded/types/sequencer.py
+++ src/encoded/types/sequencer.py
@@ -1,4 +1,7 @@
-from snovault import collection, load_schema
+from typing import Optional
+
+from pyramid.request import Request
+from snovault import calculated_property, collection, display_title_schema, load_schema
 
 from .base import Item
 
@@ -15,3 +18,19 @@ class Sequencer(Item):
     item_type = "sequencer"
     schema = load_schema("encoded:schemas/sequencer.json")
     embedded_list = []
+
+    @calculated_property(schema=display_title_schema)
+    def display_title(
+        self,
+        request: Request,
+        platform: Optional[str],
+        model: Optional[str],
+        identifier: Optional[str],
+        accession: Optional[str],
+    ) -> str:
+        if platform and model:
+            return f"{platform} {model}"
+        if identifier:
+            return identifier
+        if accession:
+            return accession
