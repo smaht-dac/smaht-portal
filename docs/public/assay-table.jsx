76b8857fabf3c6de7ed02e52b383c53a17f6090d
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2024-01-30 14:15:52 -0500

    -feat: separate alluvial and assay table -feat: create new static section, add to /data -feat: add copy -style: pull out table styles, add disclaimer for table

---
+++ docs/public/assay-table.jsx
@@ -0,0 +1,12 @@
+<>
+    <p>
+        Below Is a summary of the sequencing plattorms to be used in the
+        benchmarking experiments by each GCC
+    </p>
+    <p>
+        Discover more about assays used by each GCC by hovering over the
+        numbers in the table. If you want to learn more about each
+        assay, please hover over the assay name to read more.
+    </p>
+    <StackRowTable />
+</>
