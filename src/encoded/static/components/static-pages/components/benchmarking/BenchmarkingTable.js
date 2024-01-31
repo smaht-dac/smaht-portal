257764bcc05325decd16c9fac429190ecce39fd8
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2024-01-31 09:09:42 -0500

    Force reload of table on login/out

--- src/encoded/static/components/static-pages/components/benchmarking/BenchmarkingTable.js
+++ src/encoded/static/components/static-pages/components/benchmarking/BenchmarkingTable.js
@@ -179,6 +179,7 @@ const BenchmarkingTable = (props) => {
 
     return (
         <EmbeddedItemSearchTable
+            key={session}
             embeddedTableHeader={
                 <BenchmarkingAboveTableComponent
                     {...{
