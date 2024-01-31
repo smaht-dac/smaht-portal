880bb547f23d8797b5c31e5ad9402570330980b1
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2024-01-31 14:17:56 -0500

    Remove random ipsum; just incase

--- docs/public/benchmarking_intro.jsx
+++ docs/public/benchmarking_intro.jsx
@@ -1,17 +1,6 @@
 <BenchmarkingUI>
     <h3>What is Benchmarking?</h3>
     <p className='readable'>
-        Eget duis at tellus at urna condimentum mattis pellentesque. 
-        Elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at. 
-        Eget aliquet nibh praesent tristique magna sit amet purus gravida. 
-        Ipsum dolor sit amet consectetur adipiscing elit duis.
+        Learn more about the various Cell Line and Benchmarking Tissue data sets by selecting a link to the left.
     </p>
-    <p className='readable'>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Pretium nibh ipsum consequat nisl. Eu turpis egestas pretium aenean.
-        Bibendum neque egestas congue quisque egestas diam in.
-    </p>
-    <hr/>
-    <p className='readable'>Click a link to the left to view data.</p>
 </BenchmarkingUI>
