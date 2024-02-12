d02b3fe03da3613a2c0d9186e7d2f3ce214fd60c
William Ronchetti <wrr33@cornell.edu>
2024-02-12 11:12:42 -0500

    href calcprop

--- src/encoded/types/quality_metric.py
+++ src/encoded/types/quality_metric.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
-from snovault import collection, load_schema
+from snovault import collection, load_schema, calculated_property
 from encoded_core.qc_views import download as qc_download
 
 from .acl import ONLY_ADMIN_VIEW_ACL
@@ -17,6 +17,16 @@ class QualityMetric(Item):
     item_type = 'quality_metric'
     schema = load_schema("encoded:schemas/quality_metric.json")
     embedded_list = []
+
+    @calculated_property(
+        schema={
+            "title": "Download URL",
+            "type": "string",
+            "description": "Use this link to download the QualityMetric zip archive.",
+        }
+    )
+    def href(self, request) -> str:
+        return f"{request.resource_path(self)}@@download/{self.uuid}"
 
 
 @view_config(name='download', context=QualityMetric, request_method='GET',
