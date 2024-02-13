980db5e82c9519e538183df6d288c9a876764368
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-02-13 15:01:44 -0500

    Add upgraders + clean file props and enums

--- src/encoded/upgrade/cell_culture.py
+++ src/encoded/upgrade/cell_culture.py
@@ -4,7 +4,7 @@ from snovault import upgrade_step
 
 
 @upgrade_step("cell_culture", "1", "2")
-def upgrade_cell_culture_1_2(value: Dict[str, Any], system: Dict) -> None:
+def upgrade_cell_culture_1_2(value: Dict[str, Any], system: Dict[str, Any]) -> None:
     """Upgrade lot number from string to list of strings."""
     existing_lot_number = value.get("lot_number")
     if existing_lot_number:
