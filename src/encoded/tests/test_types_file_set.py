ff2f6bf174b2a3d15c4dde8368a5935fef977a80
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-02-22 12:06:41 -0500

    Reformat

--- src/encoded/tests/test_types_file_set.py
+++ src/encoded/tests/test_types_file_set.py
@@ -7,7 +7,5 @@ from .utils import get_search
 @pytest.mark.workbook
 def test_files_rev_link(es_testapp: TestApp, workbook: None) -> None:
     """Ensure files rev link works."""
-    file_set_search = get_search(
-        es_testapp, "?type=FileSet&files.uuid!=No+value"
-    )
+    file_set_search = get_search(es_testapp, "?type=FileSet&files.uuid!=No+value")
     assert file_set_search
