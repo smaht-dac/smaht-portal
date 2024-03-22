77127182aabf1937dfaacbae8c28d75b495cebc7
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-03-22 14:14:32 -0400

    Refactor to share with OutputFile

--- src/encoded/types/submitted_file.py
+++ src/encoded/types/submitted_file.py
@@ -16,6 +16,7 @@ from .file import (
     FILE_EDIT_PUT_VALIDATORS,
     FILE_EDIT_UNVALIDATED_PATCH_VALIDATORS,
     FILE_EDIT_UNVALIDATED_PUT_VALIDATORS,
+    CalcPropConstants,
     File,
 )
 from .submitted_item import (
@@ -75,400 +76,75 @@ class SubmittedFile(File, SubmittedItem):
 
     Collection = SubmittedFileCollection
 
-    @calculated_property(
-        schema={
-            "title": "Sequencing",
-            "description": "Sequencing associated with the file",
-            "type": "array",
-            "uniqueItems": True,
-            "items": {
-                "type": "string",
-                "linkTo": "Sequencing",
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.LIBRARIES_SCHEMA)
+    def libraries(
+        self, request: Request, file_sets: Optional[List[str]] = None
+    ) -> Union[List[str], None]:
+        """Get Libraries associated with the file."""
+        return self._get_libraries(request, file_sets=file_sets)
+
+    @calculated_property(schema=CalcPropConstants.SEQUENCINGS_SCHEMA)
     def sequencing(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[List[str], None]:
         """Get Sequencing items associated with the file."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            result = file_utils.get_sequencings(request_handler, self.properties)
-        return result or None
+        return self._get_sequencing(request, file_sets=file_sets)
 
-    @calculated_property(
-        schema={
-            "title": "Assays",
-            "description": "Assays associated with the file",
-            "type": "array",
-            "uniqueItems": True,
-            "items": {
-                "type": "string",
-                "linkTo": "Assay",
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.ASSAYS_SCHEMA)
     def assays(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[List[str], None]:
         """Get Assays associated with the file."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            result = file_utils.get_assays(request_handler, self.properties)
-        return result or None
+        return self._get_assays(request, file_sets=file_sets)
 
-    @calculated_property(
-        schema={
-            "title": "Analytes",
-            "description": "Analytes associated with the file",
-            "type": "array",
-            "uniqueItems": True,
-            "items": {
-                "type": "string",
-                "linkTo": "Analyte",
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.ANALYTES_SCHEMA)
     def analytes(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[List[str], None]:
         """Get Analytes associated with the file."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            result = file_utils.get_analytes(request_handler, self.properties)
-        return result or None
+        return self._get_analytes(request, file_sets=file_sets)
 
-    @calculated_property(
-        schema={
-            "title": "Samples",
-            "description": "Samples associated with the file",
-            "type": "array",
-            "uniqueItems": True,
-            "items": {
-                "type": "string",
-                "linkTo": "Sample",
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.SAMPLES_SCHEMA)
     def samples(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[List[str], None]:
         """Get Samples associated with the file."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            result = file_utils.get_samples(request_handler, self.properties)
-        return result or None
+        return self._get_samples(request, file_sets=file_sets)
 
-    @calculated_property(
-        schema={
-            "title": "Sample Sources",
-            "description": "Sample sources (e.g. cell lines or tissues) associated with the file",
-            "type": "array",
-            "uniqueItems": True,
-            "items": {
-                "type": "string",
-                "linkTo": "Tissue",
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.SAMPLE_SOURCES_SCHEMA)
     def sample_sources(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[List[str], None]:
         """Get SampleSources associated with the file."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            result = file_utils.get_sample_sources(request_handler, self.properties)
-        return result or None
+        return self._get_sample_sources(request, file_sets=file_sets)
 
-    @calculated_property(
-        schema={
-            "title": "Donors",
-            "description": "Donors associated with the file",
-            "type": "array",
-            "uniqueItems": True,
-            "items": {
-                "type": "string",
-                "linkTo": "Donor",
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.DONORS_SCHEMA)
     def donors(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[List[str], None]:
         """Get Donors associated with the file."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            result = file_utils.get_donors(request_handler, self.properties)
-        return result or None
+        return self._get_donors(request, file_sets=file_sets)
 
-    @calculated_property(
-        schema={
-            "title": "File Summary",
-            "type": "object",
-            "properties": {
-                "annotated_name": {
-                    "title": "Annotated Name",
-                    "type": "string",
-                },
-                "access_status": {
-                    "title": "Access",
-                    "type": "string",
-                },
-                "uuid": {
-                    "title": "UUID",
-                    "type": "string",
-                },
-                "file_format": {
-                    "title": "Data Format",
-                    "type": "string",
-                },
-                "file_size": {
-                    "title": "Size",
-                    "type": "string",
-                },
-                "md5sum": {
-                    "title": "MD5 Checksum",
-                    "type": "string",
-                },
-                "consortia": {
-                    "title": "Consortium",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.FILE_SUMMARY_SCHEMA)
     def file_summary(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[Dict[str, Any], None]:
         """Get file summary for display on file overview page."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            file_summary = self._get_file_summary(
-                request_handler, self.properties, self.uuid
-            )
-            if file_summary:
-                return file_summary
-        return
+        return self._get_file_summary(request, file_sets=file_sets)
 
-    def _get_file_summary(
-        self,
-        request_handler: RequestHandler,
-        file_properties: Dict[str, Any],
-        uuid: str,
-    ) -> Dict[str, Any]:
-        """Get file summary for display on file overview page."""
-        to_include = {
-            "annotated_name": file_utils.get_annotated_filename(file_properties),
-            "access_status": file_utils.get_access_status(file_properties),
-            "file_format": get_property_value_from_identifier(
-                request_handler,
-                file_utils.get_file_format(file_properties),
-                item_utils.get_display_title,
-            ),
-            "file_size": file_utils.get_file_size(file_properties),
-            "md5sum": file_utils.get_md5sum(file_properties),
-            "consortia": get_property_values_from_identifiers(
-                request_handler,
-                item_utils.get_consortia(file_properties),
-                item_utils.get_display_title,
-            ),
-            "uuid": uuid,
-        }
-        return {
-            key: value for key, value in to_include.items() if value
-        }
-
-    @calculated_property(
-        schema={
-            "title": "Data Generation Summary",
-            "description": "Summary of data generation",
-            "type": "object",
-            "properties": {
-                "data_category": {
-                    "title": "Data Category",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "data_type": {
-                    "title": "Data Type",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "sequencing_center": {
-                    "title": "Sequencing Centers",
-                    "type": "string",
-                },
-                "submission_centers": {
-                    "title": "Generated By",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "assays": {
-                    "title": "Experimental Assay",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "sequencing_platforms": {
-                    "title": "Sequencing Platform",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.DATA_GENERATION_SCHEMA)
     def data_generation_summary(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[Dict[str, Any], None]:
         """Get data generation summary for display on file overview page."""
-        request_handler = RequestHandler(request=request)
-        data_generation_summary = self._get_data_generation_summary(
-            request_handler, self.properties
-        )
-        if data_generation_summary:
-            return data_generation_summary
-        return
+        return self._get_data_generation_summary(request, self.properties)
 
-    def _get_data_generation_summary(
-        self, request_handler: RequestHandler, file_properties: Dict[str, Any]
-    ) -> Dict[str, Any]:
-        """Get data generation summary for display on file overview page."""
-        to_include = {
-            "data_category": file_utils.get_data_category(file_properties),
-            "data_type": file_utils.get_data_type(file_properties),
-            "sequencing_center": get_property_value_from_identifier(
-                request_handler,
-                file_utils.get_sequencing_center(file_properties),
-                item_utils.get_display_title,
-            ),
-            "submission_centers": get_property_values_from_identifiers(
-                request_handler,
-                item_utils.get_submission_centers(file_properties),
-                item_utils.get_display_title,
-            ),
-            "assays": get_property_values_from_identifiers(
-                request_handler,
-                file_utils.get_assays(request_handler, file_properties),
-                item_utils.get_display_title,
-            ),
-            "sequencing_platforms": get_property_values_from_identifiers(
-                request_handler,
-                file_utils.get_sequencings(request_handler, file_properties),
-                item_utils.get_display_title,
-            ),
-        }
-        return {
-            key: value for key, value in to_include.items() if value
-        }
-
-    @calculated_property(
-        schema={
-            "title": "Sample Summary",
-            "type": "object",
-            "properties": {
-                "donor_ids": {
-                    "title": "Donor ID",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "tissues": {
-                    "title": "Tissue",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "sample_names": {
-                    "title": "Sample ID",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "sample_descriptions": {
-                    "title": "Description",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "analytes": {
-                    "title": "Analyte",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-                "studies": {
-                    "title": "Study",
-                    "type": "array",
-                    "items": {
-                        "type": "string",
-                    },
-                },
-            },
-        },
-    )
+    @calculated_property(schema=CalcPropConstants.SAMPLE_SUMMARY_SCHEMA)
     def sample_summary(
         self, request: Request, file_sets: Optional[List[str]] = None
     ) -> Union[Dict[str, Any], None]:
         """Get sample summary for display on file overview page."""
-        if file_sets:
-            request_handler = RequestHandler(request=request)
-            sample_summary = self._get_sample_summary(request_handler, self.properties)
-            if sample_summary:
-                return sample_summary
-        return
-
-    def _get_sample_summary(
-        self, request_handler: RequestHandler, file_properties: Dict[str, Any]
-    ) -> Dict[str, Any]:
-        """Get sample summary for display on file overview page."""
-        to_include = {
-            "donor_ids": get_property_values_from_identifiers(
-                request_handler,
-                file_utils.get_donors(request_handler, file_properties),
-                donor_utils.get_id,
-            ),
-            "tissues": [],  # TODO: Implement once tissue name added with TPC updates
-            "sample_names": get_property_values_from_identifiers(
-                request_handler,
-                file_utils.get_samples(request_handler, file_properties),
-                functools.partial(sample_utils.get_sample_names, request_handler),
-            ),
-            "analytes": get_property_values_from_identifiers(
-                request_handler,
-                file_utils.get_analytes(request_handler, file_properties),
-                analyte_utils.get_molecule,
-            ),
-            "sample_descriptions": get_property_values_from_identifiers(
-                request_handler,
-                file_utils.get_samples(request_handler, file_properties),
-                functools.partial(
-                    sample_utils.get_sample_descriptions, request_handler
-                ),
-            ),
-            "studies": get_property_values_from_identifiers(
-                request_handler,
-                file_utils.get_samples(request_handler, file_properties),
-                functools.partial(sample_utils.get_studies, request_handler),
-            ),
-        }
-        return {key: value for key, value in to_include.items() if value}
+        return self._get_sample_summary(request, self.properties)
 
     @calculated_property(condition=show_upload_credentials, schema={
         "type": "object",
