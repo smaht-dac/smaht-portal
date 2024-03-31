7d7af3ce6fbf198c11dc56faf6398155f5d4def3
David Michaels <david_michaels@hms.harvard.edu>
2024-03-31 06:34:05 -0400

    ingester progress tracking work

--- src/encoded/project/ingestion.py
+++ src/encoded/project/ingestion.py
@@ -1,3 +1,4 @@
+import json
 from dcicutils.progress_constants import PROGRESS_INGESTER
 from snovault.project.ingestion import SnovaultProjectIngestion
 
@@ -10,5 +11,22 @@ class SMaHTProjectIngestion(SnovaultProjectIngestion):
         # Importing this at the top results in the dreaded errors about app_project not being initialized.
         from encoded.ingestion.ingestion_status_cache import IngestionStatusCache
         ingestion_status = IngestionStatusCache.connection(submission_uuid, context)
-        # TODO: Put this string in dciciutils.progress_constants
         ingestion_status.update({PROGRESS_INGESTER.QUEUED: PROGRESS_INGESTER.NOW()})
+
+    def note_post_ingestion(self, message: dict, context):
+        # Example message:
+        # {
+        #     "MessageId": "f6a54ed2-7452-4478-9558-ffe8984cf004",
+        #     "ReceiptHandle": "AQEBnFA5wm6wXi-etcetera",
+        #     "MD5OfBody": "717efffebb503df7a3905ffc4d614aa6",
+        #     "Body": "{\"ingestion_type\": \"metadata_bundle\", \"uuid\": \"654ac255-0791-4c84-8e71-7be608899cef\",
+        #               \"timestamp\": \"2024-03-31T09:49:23.209312\"}"
+        # }
+        from encoded.ingestion.ingestion_status_cache import IngestionStatusCache
+        try:
+            message = json.loads(message["Body"])
+            submission_uuid = message["uuid"]
+            ingestion_status = IngestionStatusCache.connection(submission_uuid, context)
+            ingestion_status.update({PROGRESS_INGESTER.QUEUE_CLEANUP: PROGRESS_INGESTER.NOW()})
+        except Exception:
+            pass
