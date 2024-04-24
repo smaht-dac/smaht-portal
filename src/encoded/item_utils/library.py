24aeff430ae550e6c47ec7be3d4b64976353d71f
Douglas Rioux <douglas_rioux@hms.harvard.edu>
2024-04-24 16:26:56 -0400

    Fix type hints for links

--- src/encoded/item_utils/library.py
+++ src/encoded/item_utils/library.py
@@ -1,11 +1,11 @@
-from typing import Any, Dict
+from typing import Any, Dict, Union
 
 
-def get_assay(library: Dict[str, Any]) -> str:
+def get_assay(library: Dict[str, Any]) -> Union[str, Dict[str, Any]]:
     """Get assay connected to library."""
     return library.get("assay", "")
 
 
-def get_analyte(library: Dict[str, Any]) -> str:
+def get_analyte(library: Dict[str, Any]) -> Union[str, Dict[str, Any]]:
     """Get analyte connected to library."""
     return library.get("analyte", "")
