213488f924d8451ca8089ba3745ee646937f3116
Luke Nicholson <sarah_nicholson@hms.harvard.edu>
2024-05-20 13:59:28 -0700

    feat: Adds files for new_type

--- src/encoded/tests/test_schema_new_type.py
+++ src/encoded/tests/test_schema_new_type.py
@@ -6,32 +6,32 @@ from dcicutils.schema_utils import get_property
 from .utils import load_schema, validate_schema, post_item
 
 
-@pytest.fixture
-def new_type(
-    testapp: TestApp,
-    test_consortium: Dict[str, Any],
-    test_submission_center: Dict[str, Any],
-) -> Dict[str, Any]:
-    item = {
-        "consortia": [test_consortium["uuid"]],
-        "submission_centers": [test_submission_center["uuid"]],
-        "last_modified": "2018-11-13T20:20:39+00:00",
-        "date": "2024-05-17",
-        "foo_or_bar": "Bar",
-        "integer_4_to_50": 31,
-        "object_with_add_properties": {
-            "key1" : 1,
-            "key2" : "2"
-        },
-        "object_without_add_properties": {
-            "key1" : 1,
-            "key2" : "2"
-        },
-        "number_string": "1234",
-        "unique_array": ['a','b','c'],
-        "urls": "https://github.com/smaht-dac/smaht-portal",
-    }
-    return post_item(testapp, item, "New Type")
+# @pytest.fixture
+# def new_type(
+#     testapp: TestApp,
+#     test_consortium: Dict[str, Any],
+#     test_submission_center: Dict[str, Any],
+# ) -> Dict[str, Any]:
+#     item = {
+#         "consortia": [test_consortium["uuid"]],
+#         "submission_centers": [test_submission_center["uuid"]],
+#         "last_modified": "2018-11-13T20:20:39+00:00",
+#         "date": "2024-05-17",
+#         "foo_or_bar": "Bar",
+#         "integer_4_to_50": 31,
+#         "object_with_add_properties": {
+#             "key1" : 1,
+#             "key2" : "2"
+#         },
+#         "object_without_add_properties": {
+#             "key1" : 1,
+#             "key2" : "2"
+#         },
+#         "number_string": "1234",
+#         "unique_array": ['a','b','c'],
+#         "urls": "https://github.com/smaht-dac/smaht-portal",
+#     }
+#     return post_item(testapp, item, "New Type")
 
 
 @pytest.mark.parametrize(
@@ -78,6 +78,67 @@ def test_integer_4_to_50_range(integer_value: int, error_expected: bool):
         assert not validation_error
 
 
+@pytest.mark.parametrize(
+    "object_properties,error_expected",
+    [
+        ({"key1": 5, "key2": "a"}, False),
+        ({"key1": 5, "key2": "a", "key3": 7}, True),
+    ]
+)
+def test_object_without_add_properties(object_properties: dict, error_expected: bool):
+    """Test object_without_add_properties does not accept additional properties"""
+    schema = load_schema("new_type")
+    object_without_add_property = get_property(schema, "object_without_add_properties")
+    validation_error = validate_schema(object_without_add_property, object_properties)
+    if error_expected:
+        assert validation_error
+    else:
+        assert not validation_error
+
+
 # @pytest.mark.parametrize(
-#     ""
+#     "foobar_value,error_expected",
+#     [
+#         ({"is_it_bar": "Bar", "how_bar": "very"}, False),
+#         ({"is_it_bar": "Foo", "how_bar": "very"}, True),
+#     ]
 # )
+# def test_foo_or_bar_conditional(foobar_value: dict, error_expected: bool):
+#     """Tests foo_or_bar conditional property how_bar only accepted with Bar"""
+#     schema = load_schema("new_type")
+#     foo_or_bar_property = get_property(schema, "foo_or_bar")
+#     validation_error = validate_schema(foo_or_bar_property, foobar_value)
+#     if error_expected:
+#         assert validation_error
+#     else:
+#         assert not validation_error
+
+# For new_type.json
+        # "how_bar_null": {
+        #     "title": "How Bar",
+        #     "description": "If Bar, how bar?",
+        #     "type": null
+        # },
+        # "how_bar": {
+        #     "title": "Check How Bar",
+        #     "description": "Object property finds out how bar if Bar is value",
+        #     "type": "object",
+        #     "properties": {
+        #         "is_it_bar": {"$ref": "#/foo_or_bar"},
+        #         "how_bar": {"$ref": "#/how_bar_null"}
+        #     },
+        #     "if": {
+        #         "properties": {
+        #             "is_it_bar": {"const": "Bar"}
+        #         },
+        #         "required": ["is_it_bar"]
+                
+        #     },
+        #     "then": {
+        #         "properties": {
+        #             "how_bar": {
+        #                 "enum": ["very","not that much"]
+        #             }
+        #         }
+        #     },
+        # },
--- src/encoded/types/new_type.py
+++ src/encoded/types/new_type.py
@@ -2,12 +2,13 @@ from typing import Optional, Union
 from snovault import calculated_property, collection, load_schema
 from .acl import ONLY_ADMIN_VIEW_ACL
 from .base import Item
+#from pyramid.request import Request
 
 def _build_new_type_embedded_list():
     """Embeds for search on new types."""
     return [
         # Facets
-        "submission_centers.display.title",
+        "submission_centers.display_title",
         "consortia.display_title",
         "foo_or_bar",
         "object_with_add_properties.key1",
@@ -34,12 +35,20 @@ class NewType(Item):
     item_type = "new_type"
     schema = load_schema("encoded:schemas/new_type.json")
     embedded_list = _build_new_type_embedded_list()
-
-    @calculated_property(schema={"title": "Foobar Value", "type": "string"})
-    def string_and_number(self, foo_or_bar: Optional[str], integer_4_to_50: Optional[int]) -> Union[str, None]:
-        """return string of foo_or_bar and integer_4_to_50"""
-        if foo_or_bar and integer_4_to_50:
-            new_string=u'{} {}'.format(foo_or_bar, str(integer_4_to_50))
-            return new_string
+    # @calculated_property(schema={"title": "Foobar Value", "type": "string"})
+    # def string_and_number(self, foo_or_bar: Optional[str], integer_4_to_50: Optional[int]) -> Union[str, None]:
+    #     """return string of foo_or_bar and integer_4_to_50"""
+    #     if foo_or_bar and integer_4_to_50:
+    #         new_string=u'{} {}'.format(foo_or_bar, str(integer_4_to_50))
+    #         return new_string
+    # @calculated_property(
+    #     schema={
+    #         "title": "Tissue Sample",
+    #         "type": "string",
+    #         "linkTo": "TissueSample",
+    #     },
+    # )
+    # def tissue_sample(self, request: Request):
+    #     #result = self.
+    #     pass
     
-    #@calculated_property(schema)
