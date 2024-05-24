f94b7d7286225a3a43014a5763da42a046fc745f
Luke Nicholson <sarah_nicholson@hms.harvard.edu>
2024-05-24 14:10:39 -0700

    fix: Add calc prop for submission center and reverse link calc prop for donor

--- src/encoded/tests/test_types_new_type.py
+++ src/encoded/tests/test_types_new_type.py
@@ -1,58 +1,122 @@
 import pytest
+from typing import Dict, Any
 from webtest import TestApp
+from dcicutils.schema_utils import get_property
 
-from .utils import get_item
-from ..item_utils import (
-    new_type as new_type_utils,
-    item as item_utils,
-)
+from .utils import get_item, post_item, post_item_and_return_location
+# from ..item_utils import (
+#     new_type as new_type_utils,
+#     item as item_utils,
+# )
+@pytest.fixture
+def new_type(
+    testapp: TestApp,
+    test_consortium: Dict[str, Any],
+    test_submission_center: Dict[str, Any],
+    #test_tissue_sample: Dict[str, Any],
+) -> Dict[str, Any]:
+    item = {
+        "consortia": [test_consortium["uuid"]],
+        "submission_centers": [test_submission_center["uuid"]],
+        "identifier": "NT1",
+        "last_modified": {
+            "date_modified": "2018-11-13T20:20:39+00:00"
+        },
+        "date": "2024-05-17",
+        "foo_or_bar": "Bar",
+        "integer_4_to_50": 31,
+        #"tissue_samples_link": [test_tissue_sample["uuid"]],
+        "object_with_add_properties": {
+            "key1" : 1,
+            "key2" : "2"
+        },
+        "object_without_add_properties": {
+            "key1" : 1,
+            "key2" : "2"
+        },
+        "number_string": "1234",
+        "unique_array": ["a","b","c"],
+        "urls": ["https://github.com/smaht-dac/smaht-portal"],
+    }
+    return post_item(testapp, item, "NewType")
 
 
-@pytest.mark.workbook
-def test_submitted_id_resource_path(es_testapp: TestApp, workbook: None) -> None:
-    """Ensure identifier is available resource path for new type within collection.
+# Make fixture with just toy item of tissue_sample
+#@pytest.mark.workbook
+# def test_submitted_id_resource_path(testapp: TestApp) -> None:
+#     """Ensure identifier is available resource path for new type within collection.
 
-    Note: Must match a New Type 'identifier' in temp-local-inserts
-    """
-    get_item(es_testapp, "NT1", collection="NewType", status=301)
+#     Note: Must match a New Type 'identifier' in temp-local-inserts
+#     """
+#     get_item(testapp, "NT1", collection="NewType", status=301)
+
+def test_string_and_number_calc_prop(new_type: Dict[str, Any]) -> None:
+    """Ensure string_and_number calc prop gives expected value"""
+    expected="Bar 31"
+    # item = get_item(
+    #     testapp,
+    #     "NT1",
+    #     collection="NewType"
+    # )
+    string_and_number = new_type.get("string_and_number","")
+    assert string_and_number == expected
+
+def test_submission_centers(
+    testapp: TestApp,
+    new_type: Dict[str, Any]) -> None:
+    """Ensure 'submission_centers' calc prop working."""
+    expected = ["SMaHT Test GCC"]
+    submission_center_display_title = new_type.get("submission_centers_display_title","")
+
+    assert submission_center_display_title == expected
 
 
-# def test_submission_centers(
-#         es_testapp: TestApp, workbook: None) -> None:
-#     """Ensure 'submission_centers' calc prop working."""
-#     expected = ["SMaHT Test Center"]
+# def test_tissue_samples(es_testapp: TestApp, workbook: None) -> None:
+#     """Ensure 'tissue_samples' calc prop working.
+    
+#     Expected values determined here by parsing file properties/embeds
+#     """
+#     expected=["TEST_TISSUE-SAMPLE_LIVER"]
+#     # Might be 'SMHT-0001'
+#     #import pdb; pdb.set_trace()
 #     item = get_item(
 #         es_testapp,
 #         "NT1",
 #         collection="NewType"
 #     )
-#     assert item.get("submission_centers","") == expected
+#     #tissue_samples = new_type_utils.get_tissue_samples(item)
+#     display_title = item.get("tissue_samples_display_title",[])
+#     assert  display_title == expected
 
 
-def test_string_and_number_calc_prop(es_testapp: TestApp, workbook: None) -> None:
-    """Ensure string_and_number calc prop gives expected value"""
-    expected="Bar 31"
-    item = get_item(
-        es_testapp,
-        "NT1",
-        collection="NewType"
-    )
-    assert item.get("string_and_number","") == expected
+@pytest.fixture
+def donor_properties(
+    test_second_submission_center: Dict[str, Any],
+    new_type: Dict[str, Any],
+    ) -> Dict[str, Any]:
+    """Donor properties not added to database.
 
-
-def test_tissue_samples(es_testapp: TestApp, workbook: None) -> None:
-    """Ensure 'tissue_samples' calc prop working.
-    
-    Expected values determined here by parsing file properties/embeds
+    Useful for testing posting permissions.
     """
-    expected=["TEST_TISSUE-SAMPLE_LIVER"]
-    # Might be 'SMHT-0001'
-    #import pdb; pdb.set_trace()
-    item = get_item(
-        es_testapp,
-        "NT1",
-        collection="NewType"
-    )
-    #tissue_samples = new_type_utils.get_tissue_samples(item)
-    display_title = item.get("tissue_samples_display_title",[])
-    assert  display_title == expected
+    return {
+        "submission_centers": [test_second_submission_center["uuid"]],
+        "submitted_id": "SECONDTEST_DONOR_1234",
+        "age": 35,
+        "sex": "Male",
+        "has_new_type": [new_type["uuid"]]
+    }
+
+
+@pytest.fixture
+def donor(testapp: TestApp, donor_properties: Dict[str, Any]) -> Dict[str, Any]:
+    return post_item(testapp, donor_properties, "Donor")
+    #return post_item_and_return_location(testapp, item, 'new_type')
+
+def test_donor_rev_link(testapp: TestApp,
+    new_type: Dict[str, Any],
+    donor: Dict[str, Any]) -> None:
+    """Ensure 'donor' rev link calc prop working."""
+    expected = donor.get("submission_id","")
+    donor_rev_link = new_type.get("donor","")
+
+    assert donor_rev_link == expected
--- src/encoded/types/new_type.py
+++ src/encoded/types/new_type.py
@@ -3,9 +3,9 @@ from snovault import calculated_property, collection, load_schema, display_title
 from .acl import ONLY_ADMIN_VIEW_ACL
 from pyramid.request import Request
 
-from ..item_utils import (
-    new_type as new_type_utils,
-)
+# from ..item_utils import (
+#     new_type as new_type_utils,
+# )
 from .utils import get_item
 from ..item_utils.utils import (
     RequestHandler,
@@ -25,8 +25,8 @@ def _build_new_type_embedded_list() -> List[str]:
         "submission_centers.identifier",
 
         # Tissue Sample linkTo
-        "tissue_samples.submitted_id",
-        "tissue_samples.display_title"
+        #"tissue_samples.submitted_id",
+        #"tissue_samples.display_title"
     ]
 
 
@@ -44,83 +44,92 @@ class NewType(Item):
     schema = load_schema("encoded:schemas/new_type.json")
     embedded_list = _build_new_type_embedded_list()
 
-    # @calculated_property(
-    #     schema={
-    #         "title": "Submission Centers",
-    #         "type": "array",
-    #         "items": {
-    #             "type": "string",
-    #             "linkTo": "SubmissionCenter"
-    #         }
-    #     }
-    # )
-    # def submission_centers(
-    #     self, request_handler: RequestHandler,new_type_properties: Dict[str, Any], uuid: str,
-    #     ) -> Union[List[str],None]:
-    #     """Submission Centers for the new type."""
-    #     #result = self.get("submission_centers.display_title",[])
-    #     result=get_property_values_from_identifiers(
-    #             request_handler,
-    #             item_utils.get_submission_centers(new_type_properties),
-    #             item_utils.get_display_title,
-    #         )
-    #     return result
+    rev = {
+        "donor": ("Donor", "has_new_type"),
+    }
 
     @calculated_property(schema={"title": "Foobar Value", "type": "string"})
     def string_and_number(
         self, foo_or_bar: Optional[str], integer_4_to_50: Optional[int] = None
-        ) -> Union[str, int, None]:
+        ) -> str:
         """return string of foo_or_bar and integer_4_to_50"""
         if foo_or_bar and integer_4_to_50:
             new_string=u'{} {}'.format(foo_or_bar, str(integer_4_to_50))
             return new_string
         return foo_or_bar
-    
-    @calculated_property(schema={
-        "title": "Tissue Samples",
-        "type": "array",
-        "description": "Tissue Samples associated with the new type",
-        "items": {
-            "type": "string",
-            "linkTo": "TissueSample",
-        },
-    })
-    def tissue_samples_display_title(
-        self, request: Request, tissue_samples: Optional[List[str]] = None
-    ) -> Union[List[str], None]:
-        """Get Tissue Samples associated with the new type."""
+
+    @calculated_property(
+        schema={
+            "title": "Submission Centers",
+            "type": "array",
+            "items": {
+                "type": "string",
+                "linkTo": "SubmissionCenter"
+            }
+        }
+    )
+    def submission_centers_display_title(
+        self, request: Request,submission_centers: List[str],
+        ) -> Union[List[str],None]:
+        """Submission Centers for the new type."""
+        #result = self.get("submission_centers.display_title",[])
         result = None
-        if tissue_samples:
-            tissue_sample_items = [
-                get_item(request, tissue_sample, collection="TissueSample")
-                for tissue_sample in tissue_samples
+        if submission_centers:
+            submission_center_items = [
+                get_item(request, submission_center, collection="SubmissionCenter")
+                for submission_center in submission_centers
             ]
-            tissue_sample_display_titles = [
-                tissue_sample.get("display_title") for tissue_sample in tissue_sample_items
-                if tissue_sample.get("display_title")
+            submission_center_display_titles = [
+                submission_center_item.get("display_title") for submission_center_item in submission_center_items
+                if submission_center_item.get("display_title")
             ]
-            if tissue_sample_display_titles:
-                result = sorted(list(tissue_sample_display_titles))
+            if submission_center_display_titles:
+                result = sorted(list(submission_center_display_titles))
         return result
 
-    def _get_tissue_samples(
-            self, request: Request,tissue_samples: Optional[List[str]] = None,
-        ) -> List[str]:
-        """Get the  tissue samples associated with the new type."""
-        result = None
-        if tissue_samples:
-            request_handler = RequestHandler(request=request)
-            # result = get_property_values_from_identifiers(
-            #     request_handler,
-            #     new_type_utils.get_tissue_samples(self.properties, request_handler),
-            #     item_utils.get_display_title)
-            result=new_type_utils.get_tissue_samples(self.properties, request_handler)
-        return result
+
+    @calculated_property(
+        schema={
+            "title": "Donor",
+            "type": "array",
+            "items": {
+                "type": "string",
+                "linkTo": "Tissue",
+            },
+        },
+    )
+    def donor(self, request: Request) -> Union[str, None]:
+        result = self.rev_link_atids(request, "donor")
+        if result:
+            return result
+        return
     
-    # @calculated_property(
-    #     schema={}
-    # )
-    # def donor(self, request: Request):
-    #     pass
+    # @calculated_property(schema={
+    #     "title": "Tissue Samples",
+    #     "type": "array",
+    #     "description": "Tissue Samples associated with the new type",
+    #     "items": {
+    #         "type": "string",
+    #         "linkTo": "TissueSample",
+    #     },
+    # })
+    # def tissue_samples_display_title(
+    #     self, request: Request, tissue_samples: Optional[List[str]] = None
+    # ) -> Union[List[str], None]:
+    #     """Get Tissue Samples associated with the new type."""
+    #     result = None
+    #     if tissue_samples:
+    #         tissue_sample_items = [
+    #             get_item(request, tissue_sample, collection="TissueSample")
+    #             for tissue_sample in tissue_samples
+    #         ]
+    #         tissue_sample_display_titles = [
+    #             tissue_sample.get("display_title") for tissue_sample in tissue_sample_items
+    #             if tissue_sample.get("display_title")
+    #         ]
+    #         if tissue_sample_display_titles:
+    #             result = sorted(list(tissue_sample_display_titles))
+    #     return result
+
 
 
