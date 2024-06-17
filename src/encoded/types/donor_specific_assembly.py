edb1e45b277cae93b7bf7ef300da83f086b1dcce
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2024-06-17 17:09:34 -0400

    Add donor_specific_assembly item

--- src/encoded/types/donor_specific_assembly.py
+++ src/encoded/types/donor_specific_assembly.py
@@ -1,24 +1,22 @@
 from snovault import collection, load_schema
 
-from .acl import ONLY_ADMIN_VIEW_ACL
 from .submitted_item import SubmittedItem
-from .reference_genome import ReferenceGenome
 
 
 def _build_dsa_embedded_list():
     """Embeds for search on donor-specific assemblies."""
     return []
 
+
 @collection(
-    name="donor-specific-assembly",
+    name="donor-specific-assemblies",
     unique_key="donor_specific_assembly:submitted_id",
-    acl=ONLY_ADMIN_VIEW_ACL,
     properties={
-        "title": "Donor Specific Assembly",
+        "title": "Donor Specific Assemblies",
         "description": "Assembled donor-specific genomes for sequencing alignment",
     },
 )
-class DonorSpecificAssembly(SubmittedItem, ReferenceGenome):
+class DonorSpecificAssembly(SubmittedItem):
     item_type = "donor_specific_assembly"
     schema = load_schema("encoded:schemas/donor_specific_assembly.json")
     embedded_list = _build_dsa_embedded_list()
