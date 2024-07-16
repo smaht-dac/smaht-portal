2f3e0d8a9b226a6ea2f8e04050edaee10449b093
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2024-07-16 16:55:24 -0400

    Add RNA molecule Analyte validator

---
+++ src/encoded/tests/test_types_analyte.py
@@ -0,0 +1,52 @@
+import pytest
+from webtest.app import TestApp
+from typing import Dict, Any
+
+from .utils import get_insert_identifier_for_item_type, patch_item, get_item_from_search
+from .test_permissions import post_item_then_delete, post_item_to_fail
+
+@pytest.mark.workbook
+@pytest.mark.parametrize(
+    "post_body,expected_status", [
+        ({"molecule": ["RNA"],"molecule_detail": ["mRNA"]}, 200),
+        ({"molecule": ['DNA'], "rna_integrity_number": 7, "rna_integrity_number_instrument": "Agilent Bioanalyzer"},422),
+        ({"molecule": ['RNA'], "ribosomal_rna_ratio": 1.5}, 200),
+        ({"molecule": ['RNA','DNA'], "ribosomal_rna_ratio": 1.5}, 200),
+    ]
+)
+def validate_rna_molecule_properties_on_patch(
+    es_testapp: TestApp,
+    patch_body: Dict[str, Any],
+    expected_status: int
+) -> None:
+    """Ensure analyte molecule RNA properties validated on PATCH."""
+    identifier = get_insert_identifier_for_item_type(es_testapp,'analyte')
+    patch_item(es_testapp, patch_body, identifier, status=expected_status)
+
+
+@pytest.mark.workbook
+@pytest.mark.parametrize(
+    "post_body,expected_status", [
+        ({"molecule": ["RNA"],"molecule_detail": ["mRNA"]}, 200),
+        ({"molecule": ['DNA'],"molecule_detail": ["Total DNA"],"rna_integrity_number": 7, "rna_integrity_number_instrument": "Agilent Bioanalyzer"},422),
+        ({"molecule": ['RNA'],"molecule_detail": ["mRNA"], "ribosomal_rna_ratio": 1.5}, 200),
+        ({"molecule": ['RNA','DNA'],"molecule_detail": ["mRNA","Total DNA"], "ribosomal_rna_ratio": 1.5}, 200),
+    ]
+)
+def validate_rna_molecule_properties_on_post(
+    es_testapp: TestApp,
+    post_body: Dict[str, Any],
+    expected_status: int
+) -> None:
+    """Ensure analyte molecule RNA properties validated on POST."""
+    analyte_insert = get_item_from_search(es_testapp,'analyte')
+    identifying_post_body = {
+        "submitted_id": 'TEST_ANALYTE_TEST',
+        "submission_centers": analyte_insert.get("submission_centers",[]),
+        "samples": analyte_insert.get("samples",[])
+        **post_body
+    }
+    if expected_status == 422:
+        post_item_to_fail(es_testapp,"analyte",identifying_post_body)
+    elif expected_status == 200:
+        post_item_then_delete(es_testapp,es_testapp,"analyte",identifying_post_body)
--- src/encoded/types/analyte.py
+++ src/encoded/types/analyte.py
@@ -1,6 +1,20 @@
 from snovault import collection, load_schema
+from snovault.util import debug_log
 
-from .submitted_item import SubmittedItem
+from pyramid.view import view_config
+
+from .submitted_item import (
+    SUBMITTED_ITEM_ADD_VALIDATORS,
+    SUBMITTED_ITEM_EDIT_PATCH_VALIDATORS,
+    SUBMITTED_ITEM_EDIT_PUT_VALIDATORS,
+    SubmittedItem,
+)
+
+from .base import (
+    collection_add,
+    item_edit,
+    Item
+)
 
 
 @collection(
@@ -15,3 +29,67 @@ class Analyte(SubmittedItem):
     item_type = "analyte"
     schema = load_schema("encoded:schemas/analyte.json")
     embedded_list = []
+
+    class Collection(Item.Collection):
+        pass
+
+
+def validate_rna_molecule_properties(context,request):
+    """Check that `molecule` contains RNA if RNA-specific properties have values."""
+
+    rna_properties = [
+        'rna_integrity_number',
+        'rna_integrity_number_instrument',
+        'ribosomal_rna_ratio'
+    ]
+    molecule = 'RNA'
+    data = request.json
+    if 'molecule' in data:
+        if molecule not in data['molecule']:
+            for property in rna_properties:
+                if data.get(property,""):
+                    msg = f"Property {property} is specific to molecule {molecule}."
+                    return request.errors.add('body', 'Analyte: invalid property values', msg)
+                else:
+                    return request.validated.update({})
+        return request.validated.update({})
+
+
+ANALYTE_ADD_VALIDATORS = SUBMITTED_ITEM_ADD_VALIDATORS + [
+    validate_rna_molecule_properties
+]
+
+@view_config(
+    context=Analyte.Collection,
+    permission='add',
+    request_method='POST',
+    validators=ANALYTE_ADD_VALIDATORS,
+)
+@debug_log
+def analyte_add(context, request, render=None):
+    return collection_add(context, request, render)
+
+
+ANALYTE_EDIT_PATCH_VALIDATORS = SUBMITTED_ITEM_EDIT_PATCH_VALIDATORS + [
+    validate_rna_molecule_properties
+]
+
+ANALYTE_EDIT_PUT_VALIDATORS = SUBMITTED_ITEM_EDIT_PUT_VALIDATORS + [
+    validate_rna_molecule_properties
+]
+
+@view_config(
+    context=Analyte,
+    permission='edit',
+    request_method='PUT',
+    validators=ANALYTE_EDIT_PUT_VALIDATORS,
+)
+@view_config(
+    context=Analyte,
+    permission='edit',
+    request_method='PATCH',
+    validators=ANALYTE_EDIT_PATCH_VALIDATORS,
+)
+@debug_log
+def analyte_edit(context, request, render=None):
+    return item_edit(context, request, render)
