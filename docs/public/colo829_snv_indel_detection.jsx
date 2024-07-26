b88afdcd97a47866d847e07223e14b9baa1f0ecd
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2024-07-26 10:03:13 -0400

    Text styling

--- docs/public/colo829_snv_indel_detection.jsx
+++ docs/public/colo829_snv_indel_detection.jsx
@@ -24,15 +24,15 @@
     <DownloadAllFilesFromSearchHrefButton {...{ session }} searchHref="/search/?type=File&dataset=colo829_snv_indel_challenge_data" cls="mt-5 float-right"/>
 
     <h3 className="section-title">Truth Sets</h3>
-    <p>The truth set in the Version 1 release includes SNVs with VAF &gt; 25% in the pure COLO829-T cancer cell line (corresponding to VAF &gt; 0.5%, expected in the BLT50 mixture samples) to analyze variants considered for the SNV/Indel Detection Challenge.</p>
+    <p className="mb-2">The truth set in the Version 1 release includes SNVs with VAF &gt; 25% in the pure COLO829-T cancer cell line (corresponding to VAF &gt; 0.5%, expected in the BLT50 mixture samples) to analyze variants considered for the SNV/Indel Detection Challenge.</p>
     <ChallengeTableWrapper searchHref="/search/?type=File&dataset=colo829_snv_indel_challenge_data&tags=truth_set" context={context} schemas={schemas} session={session} href={href}/>
 
     <h3 className="section-title">Negative Control</h3>
-    <p>For accurate estimation of precision, we derived the “negative control” set consisting of positions that are (almost certainly) not true mosaic variant sites. The negative controls include non-variant, homozygous reference positions and germline variant positions in COLO829-BL.</p>
+    <p className="mb-2">For accurate estimation of precision, we derived the “negative control” set consisting of positions that are (almost certainly) not true mosaic variant sites. The negative controls include non-variant, homozygous reference positions and germline variant positions in COLO829-BL.</p>
     <ChallengeTableWrapper searchHref="/search/?type=File&dataset=colo829_snv_indel_challenge_data&tags=negative_control" context={context} schemas={schemas} session={session} href={href} />
 
     <h3 className="section-title">Genome Stratification for Benchmarking</h3>
-    <p>We categorized genomic regions with different levels of confidence for mosaic variant detection, stratified into three groups, i.e. “Easy”, “Difficult”, and “Extreme”. We considered the easy regions in the Genome in a Bottle and expanded the genome categorization based on k-mer-based read mappability scores using the UMAP software (Karimzadeh M et al. (2018) NAR). The regions are based on the GRCh38 reference human genome.</p>
+    <p className="mb-2">We categorized genomic regions with different levels of confidence for mosaic variant detection, stratified into three groups, i.e. “Easy”, “Difficult”, and “Extreme”. We considered the easy regions in the Genome in a Bottle and expanded the genome categorization based on k-mer-based read mappability scores using the UMAP software (Karimzadeh M et al. (2018) NAR). The regions are based on the GRCh38 reference human genome.</p>
     <ChallengeTableWrapper searchHref="/search/?type=File&dataset=colo829_snv_indel_challenge_data&tags=genome_stratification" context={context} schemas={schemas} session={session} href={href}/>
 
     {/* <h3 className="section-title">Submitted VCFs & Analysis Results</h3>
