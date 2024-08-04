e9603bc6cda7612e23749f37f9d04fb4ac5e5173
David Michaels <david_michaels@hms.harvard.edu>
2024-08-04 08:42:34 -0400

    Added validators endpoint to validate (initially) submitted_id for smaht-submitr.

--- src/encoded/validators.py
+++ src/encoded/validators.py
@@ -2,6 +2,8 @@ from pyramid.view import view_config
 from snovault.util import debug_log
 from encoded.types.submitted_item import validate_submitted_id
 
+# Module with custom validators exposed as APIs.
+# First used for submitted_id validation for smaht-submitr.
 
 def includeme(config):
     config.add_route("validators", "/validators/{validator}")
@@ -9,6 +11,19 @@ def includeme(config):
 
 
 def _validators_submitted_id(context, request):
+    """
+    Given 'value' and 'submission_centers' arguments, returns a dictionary with an indication
+    of whether or not the value which represents a submitted_id is of valid format, asssuming
+    that submission_centers contains the comma-separated list of known submission center names,
+    e.g. washu_gcc. If the submitted_id is valid then returns something like:
+
+    {"submitted_id": "BCM_SOFTWARE_LIMA", "status": "OK"}
+
+    If the submitted_id is not valid then returns something like:
+
+    {"submitted_id": "XYZ_SOFTWARE_LIMA",
+     "status": "Submitted ID BCM_SOFTWARE_LIMA start (XYZ) does not match options for submission centers: ['DAC', 'WASHU']"}
+    """
     if (submitted_id := request.GET.get("value")) and (submission_centers := request.GET.get("submission_centers")):
         # Note that passing submission_centers (the known/valid set of submission-center names, e.g. smaht_dac)
         # is done for more control and for performance so we don't have to get the set of submission centers here.
