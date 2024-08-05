e174b2ab8ddb53ff159a8c4ace164e263e7e63e2
David Michaels <david_michaels@hms.harvard.edu>
2024-08-05 17:07:14 -0400

    Added validators endpoint to validate (initially) submitted_id for smaht-submitr.

--- src/encoded/validators.py
+++ src/encoded/validators.py
@@ -24,13 +24,26 @@ def _validators_submitted_id(context, request):
     {"submitted_id": "XYZ_SOFTWARE_LIMA",
      "status": "Submitted ID BCM_SOFTWARE_LIMA start (XYZ) does not match options for submission centers: ['DAC', 'WASHU']"}
     """
-    if (submitted_id := request.GET.get("value")) and (submission_centers := request.GET.get("submission_centers")):
-        # Note that passing submission_centers (the known/valid set of submission-center names, e.g. smaht_dac)
-        # is done for more control and for performance so we don't have to get the set of submission centers here.
-        submission_centers = [item.strip() for item in submission_centers.split(",")]
+    EFFECTIVE_PRINCIPALS_SUBMITS_FOR_PREFIX = "submits_for."
+    STATUS_OK = "OK"
+    if submitted_id := request.GET.get("value"):
+        if (submission_centers := request.GET.get("submission_centers")):
+            # Allow passing in just in case we need it.
+            submission_centers = [item.strip() for item in submission_centers.split(",")]
+        elif "group.admin" in request.effective_principals:
+            # For admin users we let anything be valid.
+            return {"submitted_id": submitted_id, "status": STATUS_OK}
+        elif (isinstance(request.effective_principals, list) and
+              (submits_for := [item[len(EFFECTIVE_PRINCIPALS_SUBMITS_FOR_PREFIX):]
+                               for item in request.effective_principals
+                               if item.startswith(EFFECTIVE_PRINCIPALS_SUBMITS_FOR_PREFIX)])):
+            # Common case: Look for effective_principals list for "submits_for.{submission_center_uuid}". 
+            submission_centers = _lookup_submission_center_codes(request, submits_for)
+        else:
+            submission_centers = []
         result = validate_submitted_id(request, submitted_id=submitted_id, submission_centers=submission_centers)
         if result is None:
-            status = "OK"
+            status = STATUS_OK
         elif (isinstance(error_detail := result.detail, dict) and 
               isinstance(error_description := error_detail.get("description"), str)):
             status = error_description
@@ -40,10 +53,20 @@ def _validators_submitted_id(context, request):
     return {}
 
 
+def _lookup_submission_center_codes(request, submission_centers):
+    submission_center_codes = []
+    for submission_center in submission_centers:
+        # TODO: Do we need an as_user="INGESTION" argument or something with the request.embed call below?
+        if submission_center := request.embed(f"/submission-centers/{submission_center}"):
+            if submission_center_code := submission_center.get("identifier"):
+                submission_center_codes.append(submission_center_code)
+        pass
+    return submission_center_codes
+
+
 _VALIDATORS = {
     "submitted_id": _validators_submitted_id
 }
-
 
 @view_config(route_name="validators", request_method=["GET"])
 @debug_log
