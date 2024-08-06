91c95a3b9d123e217b075a04b412495a5bfc6f12
David Michaels <david_michaels@hms.harvard.edu>
2024-08-06 08:12:54 -0400

    Added validators endpoint to validate (initially) submitted_id for smaht-submitr.

--- src/encoded/validators.py
+++ src/encoded/validators.py
@@ -6,41 +6,50 @@ from encoded.types.submitted_item import validate_submitted_id
 # First used for submitted_id validation for smaht-submitr.
 
 def includeme(config):
-    config.add_route("validators", "/validators/{validator}")
+    config.add_route("validators", "/validators/{validator}/{value}")
     config.scan(__name__)
 
 
-def _validators_submitted_id(context, request):
+def _validator_submitted_id(context, request, value):
     """
-    Given 'value' and 'submission_centers' arguments, returns a dictionary with an indication
-    of whether or not the value which represents a submitted_id is of valid format, asssuming
-    that submission_centers contains the comma-separated list of known submission center names,
-    e.g. washu_gcc. If the submitted_id is valid then returns something like:
+    Assuming the given value argument is a submitted_id, returns a dictionary with an indication of
+    whether or not it is of valid format. An optional request submission_centers argument may be given
+    representing a comma-separated list of submission center names (e.g. washu_gcc) which are allowed
+    to be represented within the submitted_id; if this is NOT given then the submission centers are
+    obtained from the effective_principals for the user of the request; UNLESS the user has admin
+    priviliges in which case we say that the given submitted_id is valid regardless of its format.
+    If the submitted_id is valid then returns something like this:
 
-    {"submitted_id": "BCM_SOFTWARE_LIMA", "status": "OK"}
+      {"submitted_id": "BCM_SOFTWARE_LIMA", "status": "OK"}
 
-    If the submitted_id is not valid then returns something like:
+    If the submitted_id is not valid then returns something like this:
 
-    {"submitted_id": "XYZ_SOFTWARE_LIMA",
-     "status": "Submitted ID BCM_SOFTWARE_LIMA start (XYZ) does not match options for submission centers: ['DAC', 'WASHU']"}
+      {"submitted_id": "XYZ_SOFTWARE_LIMA",
+       "status": "Submitted ID BCM_SOFTWARE_LIMA start (XYZ) does not match options for submission centers: ['DAC', 'WASHU']"}
+
     """
     EFFECTIVE_PRINCIPALS_SUBMITS_FOR_PREFIX = "submits_for."
     STATUS_OK = "OK"
-    if submitted_id := request.GET.get("value"):
+    if submitted_id := value:
         if (submission_centers := request.GET.get("submission_centers")):
-            # Allow passing in just in case we need it.
+            # Allow passing in submission_centers just in case we need it explicit control.
             submission_centers = [item.strip() for item in submission_centers.split(",")]
         elif "group.admin" in request.effective_principals:
-            # For admin users we let anything be valid.
+            # For admin users we let anything be valid. Not 100% technically correct; really should
+            # be using the list of ALL known submission centers; but will get checked later for real
+            # during server-side validation (for our smaht-submitr use-case); and (described above)
+            # we can also explicitly pass in submission_centers as the allowed list.
             return {"submitted_id": submitted_id, "status": STATUS_OK}
         elif (isinstance(request.effective_principals, list) and
               (submits_for := [item[len(EFFECTIVE_PRINCIPALS_SUBMITS_FOR_PREFIX):]
                                for item in request.effective_principals
                                if item.startswith(EFFECTIVE_PRINCIPALS_SUBMITS_FOR_PREFIX)])):
-            # Common case: Look for effective_principals list for "submits_for.{submission_center_uuid}". 
+            # Common case: Get the list of allowed submission centers by looking at
+            # the effective_principals list for "submits_for.{submission_center_uuid}". 
             submission_centers = _lookup_submission_center_codes(request, submits_for)
         else:
             submission_centers = []
+        # Call the real custom validator in submitted_item.py.
         result = validate_submitted_id(request, submitted_id=submitted_id, submission_centers=submission_centers)
         if result is None:
             status = STATUS_OK
@@ -60,18 +69,17 @@ def _lookup_submission_center_codes(request, submission_centers):
         if submission_center := request.embed(f"/submission-centers/{submission_center}"):
             if submission_center_code := submission_center.get("identifier"):
                 submission_center_codes.append(submission_center_code)
-        pass
     return submission_center_codes
 
 
+# Names of validators; i.e. the {validator} part of the endpoint /validators/{validator}.
 _VALIDATORS = {
-    "submitted_id": _validators_submitted_id
+    "submitted_id": _validator_submitted_id
 }
 
 @view_config(route_name="validators", request_method=["GET"])
 @debug_log
 def validators(context, request):
-    if value := request.matchdict.get("validator"):
-        if validator := _VALIDATORS.get(value):
-            return validator(context, request)
+    if (validator := request.matchdict.get("validator")) and (validator := _VALIDATORS.get(validator)):
+        return validator(context, request, request.matchdict.get("value"))
     return {}
