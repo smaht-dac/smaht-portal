d64f0152ff871a5b4e579c97f860a77ceb7f1ffb
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2024-08-08 14:09:19 -0400

    Clean up get method

--- src/encoded/types/quality_metric.py
+++ src/encoded/types/quality_metric.py
@@ -27,22 +27,6 @@ class QualityMetric(Item):
     )
     def href(self, request) -> str:
         return f"{request.resource_path(self)}@@download/{self.uuid}"
-    
-    def get(self, name, default=None):
-        """Override method to allow submitted_id keys for quality metric.
-
-        Allows ExternalQualityMetric types to show up in search of QualityMetric collection.
-        """
-        resource = super(Item, self).get(name, None)
-        if resource is not None:
-            return resource
-        if "EXTERNAL-QUALITY-METRIC" in name:
-            resource = self.connection.get_by_unique_key("submitted_id", name)
-            if resource is not None:
-                if not self._allow_contained(resource):
-                    return default
-                return resource
-
 
 
 @view_config(name='download', context=QualityMetric, request_method='GET',
