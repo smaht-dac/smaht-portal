48a334eadb9ab06cdecd07f10fbd81d7e1a81359
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2024-08-13 14:07:14 -0400

    feat: placeholder html

--- src/encoded/static/components/item-pages/components/file-overview/VcfComparatorTable.js
+++ src/encoded/static/components/item-pages/components/file-overview/VcfComparatorTable.js
@@ -1,5 +1,22 @@
 import React from 'react';
 
 export const VcfComparatorTable = () => {
-    return <div>VcfComparatorTable</div>;
+    return (
+        <div className="vcf-comparator-table-container">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Comparator</th>
+                        <th>Software</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>File ex. 1</td>
+                        <td>Software ex. 2</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 };
