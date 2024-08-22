858a6d31af1fe593bee505f725a2a6da6a28d9d7
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2024-08-22 16:46:06 -0400

    Fix typing is item_utils/assay

--- src/encoded/item_utils/assay.py
+++ src/encoded/item_utils/assay.py
@@ -1,12 +1,12 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 
-def get_valid_sequencers(properties: Dict[str, Any]) -> str:
+def get_valid_sequencers(properties: Dict[str, Any]) -> List[str]:
     """Get valid sequencers from properties."""
     return properties.get("valid_sequencers",[])
 
 
-def get_valid_molecules(properties: Dict[str, Any]) -> str:
+def get_valid_molecules(properties: Dict[str, Any]) -> List[str]:
     """Get valid molecules from properties."""
     return properties.get("valid_molecules",[])
 
