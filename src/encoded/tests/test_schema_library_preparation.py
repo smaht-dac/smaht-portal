f3db746771638b0e4227510f7da6dfb8190580e9
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2024-09-05 14:57:14 -0400

    SN Add properties to library (#241)
    * Add properties to library
    
    * Update changelog
    
    * Update library props
    
    * Add antibody suggested enums

--- src/encoded/tests/test_schema_library_preparation.py
+++ src/encoded/tests/test_schema_library_preparation.py
@@ -24,18 +24,18 @@ def test_library_preparation(
 @pytest.mark.parametrize(
     "patch_body,status",
     [
-        ({"restriction_enzymes": ["AluI"]}, 422),
-        ({"fragmentation_method": ["Transposase"]}, 200),
+        ({"enzymes": ["AluI"]}, 422),
+        ({"fragmentation_method": ["Sonication"]}, 200),
         ({"fragmentation_method": ["Restriction Enzyme"]}, 422),
-        ({"fragmentation_method": ["Restriction Enzyme"], "restriction_enzymes": ["AluI"]}, 200),
-       ({"fragmentation_method": ["Restriction Enzyme","Transposase"], "restriction_enzymes": ["AluI"]}, 200),
-       ({"fragmentation_method": ["Transposase"], "restriction_enzymes": ["AluI"]}, 422),
+        ({"fragmentation_method": ["Restriction Enzyme"], "enzymes": ["AluI"]}, 200),
+       ({"fragmentation_method": ["Transposase"], "enzymes": ["Tn5"]}, 200),
+       ({"fragmentation_method": ["Sonication"], "enzymes": ["AluI"]}, 422),
     ],
 )
-def test_restriction_enzyme_conditional(
+def test_enzyme_conditional(
     testapp: TestApp, test_library_preparation: Dict[str, Any], patch_body: Dict[str, Any], status: int
 ) -> None:
-    """Ensure restriction_enzymes is only valid if fragmentation_method contains Restriction Enzyme."""
+    """Ensure enzymes is only valid if fragmentation_method contains Restriction Enzyme or Transposase."""
     library_prep_uuid = test_library_preparation["uuid"]
     assert patch_item(testapp, patch_body, library_prep_uuid, status=status)
 
