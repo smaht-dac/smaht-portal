b33acc7911f4c9c7c7e2efbe1b42738ac9169b0a
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2024-10-16 16:32:05 -0400

    Fix tests

--- src/encoded/tests/test_annotated_filename.py
+++ src/encoded/tests/test_annotated_filename.py
@@ -468,6 +468,7 @@ def test_get_donor_sex_and_age_parts(
         assert_filename_part_matches(result, expected, errors)
 
 
+SOME_FILE = {"data_category": ["Aligned Reads"]}
 SEQUENCER_CODE = "A"
 SOME_SEQUENCER = {"code": SEQUENCER_CODE}
 ANOTHER_SEQUENCER = {"code": "B"}
@@ -477,25 +478,26 @@ ANOTHER_ASSAY = {"code": "002"}
 
 
 @pytest.mark.parametrize(
-    "sequencers,assays,expected,errors",
+    "file,sequencers,assays,expected,errors",
     [
-        ([], [], "", True),
-        ([SOME_SEQUENCER], [], "", True),
-        ([], [SOME_ASSAY], "", True),
-        ([SOME_SEQUENCER], [SOME_ASSAY], f"{SEQUENCER_CODE}{ASSAY_CODE}", False),
-        ([SOME_SEQUENCER, ANOTHER_SEQUENCER], [SOME_ASSAY], "", True),
-        ([SOME_SEQUENCER], [SOME_ASSAY, ANOTHER_ASSAY], "", True),
-        ([SOME_SEQUENCER, SOME_ITEM], [SOME_ASSAY], "", True),
+        (SOME_FILE,[], [], "", True),
+        (SOME_FILE,[SOME_SEQUENCER], [], "", True),
+        (SOME_FILE,[], [SOME_ASSAY], "", True),
+        (SOME_FILE,[SOME_SEQUENCER], [SOME_ASSAY], f"{SEQUENCER_CODE}{ASSAY_CODE}", False),
+        (SOME_FILE,[SOME_SEQUENCER, ANOTHER_SEQUENCER], [SOME_ASSAY], "", True),
+        (SOME_FILE,[SOME_SEQUENCER], [SOME_ASSAY, ANOTHER_ASSAY], "", True),
+        (SOME_FILE,[SOME_SEQUENCER, SOME_ITEM], [SOME_ASSAY], "", True),
     ],
 )
 def test_get_sequencing_and_assay_codes(
+    file: Dict[str, Any],
     sequencers: List[Dict[str, Any]],
     assays: List[Dict[str, Any]],
     expected: str,
     errors: bool,
 ) -> None:
     """Test sequencing and assay codes retrieval for annotated filenames."""
-    result = get_sequencing_and_assay_codes(sequencers, assays)
+    result = get_sequencing_and_assay_codes(file, sequencers, assays)
     assert_filename_part_matches(result, expected, errors)
 
 
