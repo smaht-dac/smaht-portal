5c155f7713225f288f34c0b0a50873982343aac8
Serkan Utku Öztürk <49978017+utku-ozturk@users.noreply.github.com>
2024-11-20 22:42:06 +0300

    add projectId to cypress config

--- deploy/post_deploy_testing/cypress.config.js
+++ deploy/post_deploy_testing/cypress.config.js
@@ -1,19 +1,22 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
+    projectId: '5p1xo7',
     defaultCommandTimeout: 60000,
     pageLoadTimeout: 120000,
     requestTimeout: 40000,
     responseTimeout: 120000,
-    blockHosts: 'www.google-analytics.com', // Don't want to confuse test-views/clicks with real ones
+    blockHosts: 'www.google-analytics.com',
+    video: false,
+    chromeWebSecurity: false,
     e2e: {
-        baseUrl: 'https://data.smaht.org/',
-        specPattern: './cypress/e2e/*.cy.js',
-        // setupNodeEvents(on, config) {
-        //     // implement node event listeners here
-        // },
-        experimentalStudio: true,
-        testIsolation: "off",
-        experimentalSessionAndOrigin: true,
+        // We've imported your old cypress plugins here.
+        // You may want to clean this up later by importing these.
+        setupNodeEvents(on, config) {
+            return require('./cypress/plugins/index.js')(on, config);
+        },
+        baseUrl: 'https://data.smaht.org',
+        specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+        testIsolation: false
     },
 });
