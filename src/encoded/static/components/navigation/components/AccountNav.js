0effc82c132b6efeb8b0a61b92ca7cc5c041a014
utku-ozturk <utku.ozturk@karyasmd.com>
2024-12-10 09:36:28 +0300

    add creating an account doc link into login popup

--- src/encoded/static/components/navigation/components/AccountNav.js
+++ src/encoded/static/components/navigation/components/AccountNav.js
@@ -66,12 +66,15 @@ export const AccountNav = React.memo(function AccountNav(props) {
     const { session, updateAppSessionState, schemas, ...passProps } = props;
     const { windowWidth, href } = passProps;
 
+    const auth0PopupText = '<p><span class="text-danger fw-bold">NOTE</span> - If you are logging-in for the first time, please <a href="https://data.smaht.org/docs/access/creating-an-account" target="_blank">READ THIS DOCUMENT PAGE</a> about creating your account!<p>';
+
+
     if (!session) {
         // Render login button
         return (
             <div className="navbar-nav navbar-acct">
                 <HelpdeskButton />
-                <LoginController {...{ updateAppSessionState, auth0Options }}>
+                <LoginController {...{ updateAppSessionState, auth0Options, auth0PopupText }}>
                     <LoginNavItem
                         {...{ schemas, session, href, windowWidth }}
                         key="login-register"
