412bee696f6d0cfef5883be76701ddb69cbcb705
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2025-02-03 15:10:12 -0500

    -style: prevent text overflow for titles -feat: tooltips field for values with placeholders

--- src/encoded/static/components/item-pages/components/file-overview/FileViewDataCards.js
+++ src/encoded/static/components/item-pages/components/file-overview/FileViewDataCards.js
@@ -19,17 +19,21 @@ const DataCard = ({ header = '', data = [] }) => {
                 <span className="header-text">{header}</span>
             </div>
             <div className="body">
-                {data.map(({ title, value = null }, i) => {
+                {data.map(({ title, value = null, tooltips = null }, i) => {
                     return (
                         <div className="datum" key={i}>
-                            <span className="datum-title">{title}</span>
-                            <span
+                            <div className="datum-title">
+                                <span>{title}</span>
+                            </div>
+                            <div
                                 className={
                                     'datum-value' +
                                     (value === null ? ' coming-soon' : '')
                                 }>
-                                {value ?? 'N/A'}
-                            </span>
+                                <span data-tip={tooltips?.[value] ?? ''}>
+                                    {value ?? 'N/A'}
+                                </span>
+                            </div>
                         </div>
                     );
                 })}
@@ -169,6 +173,12 @@ const default_sample_information = [
         title: 'Description',
         getProp: (context = {}) =>
             context?.sample_summary?.sample_descriptions?.join(', '),
+        tooltips: {
+            Homogenate: 'Homogenate tooltip',
+            Specimen: 'Specimen tooltip',
+            Core: 'Core tooltip',
+            Liquid: 'Liquid tooltip',
+        },
     },
     {
         title: 'Study',
@@ -215,8 +225,12 @@ export const FileViewDataCards = ({ context = {} }) => {
             />
             <DataCard
                 header={'Sample Information'}
-                data={sample_information.map(({ title, getProp }) => {
-                    return { title, value: getProp(context) };
+                data={sample_information.map(({ title, getProp, tooltips }) => {
+                    return {
+                        title,
+                        value: getProp(context),
+                        tooltips,
+                    };
                 })}
             />
         </div>
