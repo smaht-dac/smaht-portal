74c6d02575602624b89deeedbcc31234b179fdbe
David Michaels <david_michaels@hms.harvard.edu>
2025-02-05 09:54:14 -0500

    Added support for additional SubmittedFile type to release track API i.e. /recent_released_files.

--- src/encoded/endpoints/recent_files_summary/recent_files_summary_fields.py
+++ src/encoded/endpoints/recent_files_summary/recent_files_summary_fields.py
@@ -1,16 +1,17 @@
 # These are all the possible fields on which the /recent_files_summary endpoint can aggregate by.
 # Various flags modify the specifics, for experimentation, troubleshooting, and possible future changes.
-
-AGGREGATION_FIELD_RELEASE_DATE = "file_status_tracking.released"
+# N.B. AGGREGATION_FIELD_CELL_LINE is actually not used by default (see nocells in recent_files_summary.py).
 # FYI FWIW: There is also file_sets.libraries.analytes.samples.sample_sources.display_title;
 # and that sometimes file_sets.libraries.analytes.samples.sample_sources.code does not exist.
+
+AGGREGATION_FIELD_RELEASE_DATE = "file_status_tracking.released"
 AGGREGATION_FIELD_CELL_MIXTURE = "file_sets.libraries.analytes.samples.sample_sources.code"
-AGGREGATION_FIELD_CELL_LINE = "file_sets.libraries.analytes.samples.sample_sources.cell_line.code"
 AGGREGATION_FIELD_DONOR = "donors.display_title"
+AGGREGATION_FIELD_DSA_DONOR = "donor_specific_assembly.donors,display_title"  # 2025-02-04
+AGGREGATION_FIELD_CELL_LINE = "file_sets.libraries.analytes.samples.sample_sources.cell_line.code"  # unused by default
 AGGREGATION_FIELD_FILE_DESCRIPTOR = "release_tracker_description"
 
 AGGREGATION_FIELD_GROUPING_CELL_OR_DONOR = [
     AGGREGATION_FIELD_CELL_MIXTURE,
-    AGGREGATION_FIELD_CELL_LINE,
     AGGREGATION_FIELD_DONOR
 ]
--- src/encoded/types/supplementary_file.py
+++ src/encoded/types/supplementary_file.py
@@ -7,6 +7,8 @@ def _build_supplementary_file_embedded_list():
     """Embeds for search on supplementary files."""
     return SubmittedFile.embedded_list + [
         "reference_genome.display_title",
+        "donor_specific_assembly.donors",
+        "donor_specific_assembly.cell_lines.code",
     ]
 
 
