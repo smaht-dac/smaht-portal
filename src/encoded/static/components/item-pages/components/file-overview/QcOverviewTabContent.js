65dd0cfc2400f793b313a329017d4df0b51b12f8
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2025-03-27 09:37:36 -0400

    -style: move styles up a level to reuse -feat: use d3 to format cell values

--- src/encoded/static/components/item-pages/components/file-overview/QcOverviewTabContent.js
+++ src/encoded/static/components/item-pages/components/file-overview/QcOverviewTabContent.js
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react';
 import { ajax } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
 import { getBadge } from '../../../viz/QualityMetricVisualizations/utils';
 
+import * as d3 from 'd3';
+
 // Formats the response data into a format that can be used in the table
 const formatRawData = (data) => {
+    let headers = [];
     const tableData = data.reduce((acc, qcItem) => {
         // Get the accession of the qcItem
         const accession = qcItem.accession;
+        const overall_quality_status = qcItem.overall_quality_status;
+
+        headers = [...headers, { accession, overall_quality_status }];
 
         // Loop through the qc_values of each item and save them under the item's accession
         qcItem.qc_values.forEach((qcValue) => {
@@ -46,7 +52,7 @@ const formatRawData = (data) => {
     }, {});
 
     return {
-        headers: data.map((qcItem) => qcItem.accession),
+        headers,
         tableData,
     };
 };
@@ -56,8 +62,12 @@ const QCOverviewTable = ({ qcItems }) => {
     const [data, setData] = useState(null);
 
     useEffect(() => {
+        const searchUrl = `/search/?${qcItems
+            .map((item) => `uuid=${item.uuid}`)
+            .join('&')}`;
+
         ajax.load(
-            `/search/?${qcItems.map((item) => `uuid=${item.uuid}`).join('&')}`,
+            searchUrl,
             (resp) => {
                 setData(formatRawData(resp['@graph']));
             },
@@ -70,52 +80,91 @@ const QCOverviewTable = ({ qcItems }) => {
             <table className="table table-bordered table-striped">
                 <thead>
                     <tr>
-                        <th className="text-left">Quality Metric</th>
-                        {data.headers.map((accession, i) => {
-                            return (
-                                <th className="text-left" key={i}>
-                                    Run {i + 1} (
-                                    <a href={`/${accession}`}>{accession}</a>)
-                                </th>
-                            );
-                        })}
+                        <th className="text-left w-[300px]">Quality Metric</th>
+                        {data.headers.map(
+                            ({ accession, overall_quality_status }, i) => {
+                                return (
+                                    <th
+                                        className="text-left fw-semibold"
+                                        key={i}>
+                                        <div className="d-flex flex-column">
+                                            <span className="d-flex align-items-center lh-base">
+                                                Run #{i + 1} (
+                                                <a href={`/${accession}`}>
+                                                    {accession}
+                                                </a>
+                                                )
+                                            </span>
+                                            <span className="lh-1">
+                                                Overall Quality Status:{' '}
+                                                {getBadge(
+                                                    overall_quality_status
+                                                )}
+                                            </span>
+                                        </div>
+                                    </th>
+                                );
+                            }
+                        )}
                     </tr>
                 </thead>
                 <tbody>
-                    {Object.keys(data.tableData).map((key, i) => {
-                        const { tooltip, values: rowValues = {} } =
-                            data.tableData[key];
+                    {Object.keys(data.tableData).length > 0 ? (
+                        Object.keys(data.tableData).map((key, i) => {
+                            const { tooltip, values: rowValues = {} } =
+                                data.tableData[key];
 
-                        return (
-                            <tr key={i}>
-                                {/* Title of the Quality Metric row */}
-                                <td className="text-left">
-                                    {key}
-                                    <i
-                                        className="icon icon-info-circle fas"
-                                        data-tip={tooltip}
-                                    />
-                                </td>
+                            return (
+                                <tr key={i}>
+                                    {/* Title of the Quality Metric row */}
+                                    <td className="text-left">
+                                        {key}
+                                        <i
+                                            className="icon icon-info-circle fas ms-1 text-secondary"
+                                            data-tip={tooltip}
+                                        />
+                                    </td>
 
-                                {/* Value for each column (i.e. qcItem labelled by [accession]) */}
-                                {data.headers.map((accession, i) => {
-                                    const cellValue =
-                                        rowValues[accession] ?? null;
+                                    {/* Value for each column (i.e. qcItem labelled by [accession]) */}
+                                    {data.headers.map(({ accession }, i) => {
+                                        const cellValue =
+                                            rowValues[accession] ?? null;
 
-                                    return (
-                                        <td key={i}>
-                                            {cellValue !== null ? (
-                                                <>
-                                                    {cellValue?.value}{' '}
-                                                    {getBadge(cellValue?.flag)}
-                                                </>
-                                            ) : null}
-                                        </td>
-                                    );
-                                })}
-                            </tr>
-                        );
-                    })}
+                                        return (
+                                            <td className="text-left" key={i}>
+                                                {cellValue !== null ? (
+                                                    <div className="d-flex">
+                                                        <span className="me-1 d-flex align-items-center lh-1">
+                                                            {d3.format('.3f')(
+                                                                cellValue?.value
+                                                            )}{' '}
+                                                        </span>
+                                                        <span className="lh-1">
+                                                            {getBadge(
+                                                                cellValue?.flag
+                                                            )}
+                                                        </span>
+                                                    </div>
+                                                ) : (
+                                                    <span className="text-secondary">
+                                                        -
+                                                    </span>
+                                                )}
+                                            </td>
+                                        );
+                                    })}
+                                </tr>
+                            );
+                        })
+                    ) : (
+                        <tr>
+                            <td
+                                className="text-left text-secondary"
+                                colSpan={qcItems.length + 1}>
+                                No Quality Metrics Values visible.
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </div>
@@ -129,5 +178,6 @@ const QCOverviewTable = ({ qcItems }) => {
 
 // Top level component for QC Overview tab content
 export const QcOverviewTabContent = ({ context }) => {
+    console.log('context: ', context);
     return <QCOverviewTable qcItems={context.quality_metrics} />;
 };
