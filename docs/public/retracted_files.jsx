77fc5b364acd93d4888cb1168ef2db0909232fa6
utku-ozturk <utku.ozturk@karyasmd.com>
2025-04-03 10:59:39 +0300

    retracted files header style and content updates

--- docs/public/retracted_files.jsx
+++ docs/public/retracted_files.jsx
@@ -1,1 +1,4 @@
+<p className="mb-2">
+Below are the files that had been released to the portal, and later retracted. The retracted files will not be searchable and shown in the data tables on the portal. We will include the replacement data in the table below if and when they are made available.
+</p>
 <RetractedFilesTable schemas={schemas} session={session} />
