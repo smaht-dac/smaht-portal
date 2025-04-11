37eaf18c9771163b64054987c06beb7864d9ff63
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2025-04-11 14:37:16 -0400

    Create new item

---
+++ src/encoded/types/generic_config.py
@@ -0,0 +1,22 @@
+from snovault import collection, load_schema
+
+from .acl import ONLY_ADMIN_VIEW_ACL
+from .base import Item
+
+
+@collection(
+    name='generic-configs',
+    unique_key='generic_config:identifier',
+    acl=ONLY_ADMIN_VIEW_ACL,
+    properties={
+        'title': 'Generic Config',
+        'description': 'Configurations for general use',
+    })
+class GenericConfig(Item):
+    """
+    Item type which contains a JSON `body` property and other metadata.
+    """
+    item_type = 'generic_config'
+    schema = load_schema("encoded:schemas/generic_config.json")
+    embedded_list = []
+    name_key = None
