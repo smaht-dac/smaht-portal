62617a57d9c23ea17cbb509a82d50c86cafdaa37
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2025-05-16 13:47:42 -0400

    Add tests for rev links

--- src/encoded/item_utils/exposure.py
+++ src/encoded/item_utils/exposure.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict, Union
 
 
-
-def get_category(properties: Dict[str, Any]) -> Union[int, None]:
+def get_category(properties: Dict[str, Any]) -> Union[str, None]:
     """Get category from properties."""
     return properties.get("category")
