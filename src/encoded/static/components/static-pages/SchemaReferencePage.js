482ee6d21d2954d4a58a484adf163fb60ce452d7
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2025-05-20 14:28:59 -0400

    - fix: remove table of contents - feat: add search functionality - feat: show spinner when fetching JSON

--- src/encoded/static/components/static-pages/SchemaReferencePage.js
+++ src/encoded/static/components/static-pages/SchemaReferencePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ajax } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
+import Select from 'react-select';
 
 // Renders a table of schema properties
 const SchemaPropertiesTable = ({ data = {} }) => {
@@ -24,24 +25,48 @@ const SchemaPropertiesTable = ({ data = {} }) => {
                     const item = data[propertyKey];
                     return (
                         <tr key={i}>
-                            <td
-                                className={`text-left ${
-                                    item.is_required
-                                        ? 'text-danger fw-bold'
-                                        : ''
-                                }`}>
-                                <span>{item.title ?? '-'}</span>
-                            </td>
-                            <td className="text-left">
-                                {item.description ?? '-'}
-                            </td>
-                            <td className="text-left">
-                                <code>{item.type ?? '-'}</code>
-                            </td>
-                            <td className="text-left">{item.pattern ?? '-'}</td>
-                            <td className="text-left">
-                                {item.submissionComment ?? '-'}
-                            </td>
+                            {/* Title */}
+                            {item?.title ? (
+                                <td
+                                    className={`text-left ${
+                                        item.is_required
+                                            ? 'text-danger fw-bold'
+                                            : ''
+                                    }`}>
+                                    {item.title}
+                                </td>
+                            ) : (
+                                <td className="text-left text-secondary">-</td>
+                            )}
+                            {/* Description */}
+                            {item?.description ? (
+                                <td className="text-left">
+                                    {item.description}
+                                </td>
+                            ) : (
+                                <td className="text-left text-secondary">-</td>
+                            )}
+                            {/* Type */}
+                            {item?.type ? (
+                                <td className="text-left">{item.type}</td>
+                            ) : (
+                                <td className="text-left text-secondary">-</td>
+                            )}
+                            {/* Pattern */}
+                            {item?.pattern ? (
+                                <td className="text-left">{item.pattern}</td>
+                            ) : (
+                                <td className="text-left text-secondary">-</td>
+                            )}
+                            {/* Note */}
+                            {item?.submissionComment ? (
+                                <td className="text-left">
+                                    {' '}
+                                    {item.submissionComment}
+                                </td>
+                            ) : (
+                                <td className="text-left text-secondary">-</td>
+                            )}
                         </tr>
                     );
                 })}
@@ -52,43 +77,68 @@ const SchemaPropertiesTable = ({ data = {} }) => {
     );
 };
 
-// Renders a list of schema items
+// Requests the submission schemas JSON and redners as a list of tables
 export const SchemaReferencePage = () => {
     const [schemaData, setSchemaData] = React.useState(null);
+    const [selectedSchema, setSelectedSchema] = React.useState(null);
 
     useEffect(() => {
         ajax.load(
             '/submission-schemas/?format=json',
             (resp) => {
                 // Handle the response data
-                console.log(resp);
                 setSchemaData(resp);
             },
             'GET',
             (err) => {
                 // Handle the error
-                console.error('Error fetching schema reference:', error);
+                console.error('Error fetching schema reference:', err);
             }
         );
     }, []);
 
+    const options = Object.keys(schemaData || {}).map((schemaKey) => ({
+        value: schemaKey,
+        label: schemaKey,
+    }));
+
     return schemaData ? (
         <div className="schema-reference-page">
-            {Object.keys(schemaData).map((schemaKey, i) => {
-                return (
-                    <div
-                        id={schemaKey}
-                        className={`schema-item ${schemaKey} mb-5`}
-                        key={i}>
-                        <h2>{schemaKey}</h2>
-                        <SchemaPropertiesTable
-                            data={schemaData[schemaKey]?.properties}
-                        />
-                    </div>
-                );
-            })}
+            <Select
+                value={selectedSchema}
+                // styles={customReactSelectStyleMulti}
+                onChange={(selectedItem) => {
+                    setSelectedSchema(selectedItem);
+                }}
+                options={options}
+            />
+            <div
+                className={`selected-schema schema-item ${selectedSchema?.value} mb-5`}>
+                <h2>{selectedSchema?.value}</h2>
+                <SchemaPropertiesTable
+                    data={schemaData[selectedSchema?.value]?.properties}
+                />
+            </div>
+            <hr className="my-5"></hr>
+            <div className="schemas-container">
+                {Object.keys(schemaData).map((schemaKey, i) => {
+                    return (
+                        <div
+                            id={schemaKey}
+                            className={`schema-item ${schemaKey} mb-5`}
+                            key={i}>
+                            <h2>{schemaKey}</h2>
+                            <SchemaPropertiesTable
+                                data={schemaData[schemaKey]?.properties}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     ) : (
-        <div>SchemaReferencePage</div>
+        <div>
+            <i className="icon icon-spin icon-spinner"></i>
+        </div>
     );
 };
