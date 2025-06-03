73c1fe273a7138a9fd806cf3856b65d44ada79c2
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2025-06-03 16:25:19 -0400

    SN Ontology term germ layer (#439)
    * Add germ layer ontology term inserts
    
    * Start implementing tag filter for ontology terms
    
    * Test with fixtures, change function name
    
    * Add tissue category calc prop
    
    * Fix function
    
    * Change sample_summary name
    
    * Update changelog
    
    * Add case for fibroblasts
    
    * Cleanup
    
    * Remove tags requirement

--- src/encoded/tests/test_item_utils_ontology_term.py
+++ src/encoded/tests/test_item_utils_ontology_term.py
@@ -2,32 +2,37 @@ from typing import Any, Dict, List
 
 import pytest
 from webtest import TestApp
-from .utils import get_search
-from ..item_utils.ontology_term import get_top_grouping_term
+from ..item_utils.ontology_term import get_grouping_term_from_tag
 from ..item_utils.utils import RequestHandler
 from ..item_utils.item import get_tags
 
-@pytest.mark.workbook
-def test_get_top_grouping_term(es_testapp: TestApp, workbook: None) -> None:
-    """Test function to grab top grouping term from OntologyTerm."""
 
-    ont_search = get_search(es_testapp, "?type=OntologyTerm&tags=test_terms")
-    assert ont_search
+@pytest.mark.parametrize(
+   "tag",
+    [
+        ("germ_layer"),
+        ("tissue_type"),
+    ],
+)
+def test_get_grouping_term_from_tag(
+    testapp: TestApp,
+    test_ontology_term: Dict[str, Any],
+    tag: str,
+) -> None:
+    """Test function to grab grouping term with tag from OntologyTerm."""
+    request_handler = RequestHandler(test_app=testapp)
+    expected = get_expected_grouping_term(test_ontology_term, tag)
+    result = get_grouping_term_from_tag(test_ontology_term, request_handler, tag)
+    assert result == expected
 
-    request_handler = RequestHandler(test_app=es_testapp)
-    for term in ont_search:
-        result = get_top_grouping_term(term, request_handler)
-        expected = get_expected_top_grouping_term(term)
-        assert result == expected
 
-
-def get_expected_top_grouping_term(term: Dict[str, Any]) -> List[str]:
-    """Get expected top grouping term from the ontology terms from tags.
+def get_expected_grouping_term(term: Dict[str, Any], tag: str) -> List[str]:
+    """Get expected grouping term from the ontology terms from tags.
 
     A little cheeky, but simplifies testing to have expected values
     directly on the inserts.
     """
-    expected_ont_top_tag_start = "test_top_terms-"
+    expected_ont_top_tag_start = f"{tag}_test_terms-"
     tags = get_tags(term)
     expected_ont_top_tags = [
         tag for tag in tags if tag.startswith(expected_ont_top_tag_start)
