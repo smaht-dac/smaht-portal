0c5f85d10bef4c4477d21bcb7f59d264e390db73
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2025-07-23 13:21:28 -0400

    - feat: arrange new components for public donor view - feat: add initial placeholders for Protected Data

--- src/encoded/static/components/item-pages/PublicDonorView.js
+++ src/encoded/static/components/item-pages/PublicDonorView.js
@@ -2,7 +2,7 @@
 
 import React, { useState, useEffect } from 'react';
 import _ from 'underscore';
-import { ProtectedDonorViewDataCards } from './components/donor-overview/ProtectedDonorViewDataCards';
+import { PublicDonorViewDataCards } from './components/donor-overview/PublicDonorViewDataCards';
 import DefaultItemView from './DefaultItemView';
 import { ShowHideInformationToggle } from './components/file-overview/ShowHideInformationToggle';
 import { DonorMetadataDownloadButton } from '../browse/BrowseView';
@@ -10,16 +10,16 @@ import DataMatrix from '../static-pages/components/matrix/DataMatrix';
 import { ajax } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
 
 // Page containing the details of Items of type File
-export default class ProtectedDonorOverview extends DefaultItemView {
+export default class PublicDonorOverview extends DefaultItemView {
     getTabViewContents() {
         const initTabs = [];
-        initTabs.push(ProtectedDonorView.getTabObject(this.props));
+        initTabs.push(PublicDonorView.getTabObject(this.props));
         return initTabs.concat(this.getCommonTabs()); // Add remainder of common tabs (Details, Attribution)
     }
 }
 
 // Donor Overview's header component containing breadcrumbs and filename
-const ProtectedDonorViewTitle = (props) => {
+const PublicDonorViewTitle = (props) => {
     const { context } = props;
 
     const breadcrumbs = [
@@ -58,7 +58,7 @@ const ProtectedDonorViewTitle = (props) => {
 };
 
 // Header component containing high-level information for the file item
-const ProtectedDonorViewHeader = (props) => {
+const PublicDonorViewHeader = (props) => {
     const { context = {}, session, title = null } = props;
     const { notes_to_tsv } = context;
 
@@ -72,13 +72,7 @@ const ProtectedDonorViewHeader = (props) => {
                     <div className="data-group data-row header">
                         {title}
                         <div className="d-flex gap-2 flex-wrap mt-1 mt-xl-0">
-                            <DonorMetadataDownloadButton
-                                session={session}
-                                className="col-md-auto col-12"
-                            />
-                            <div
-                                className="col-md-auto col-12"
-                                data-tip="Donor Manifest Download is coming soon">
+                            <div className="col-md-auto col-12">
                                 <button
                                     className="col-md-auto col-12 btn btn-primary btn-sm me-05 align-items-center download-button px-3"
                                     disabled>
@@ -128,7 +122,7 @@ const ProtectedDonorViewHeader = (props) => {
 };
 
 /** Top-level component for the Donor Overview Page */
-const ProtectedDonorView = React.memo(function ProtectedDonorView(props) {
+const PublicDonorView = React.memo(function PublicDonorView(props) {
     const [isLoading, setIsLoading] = useState(true);
     const [statisticValues, setStatisticValues] = useState({
         tissues: context?.tissues?.length || '-',
@@ -187,19 +181,19 @@ const ProtectedDonorView = React.memo(function ProtectedDonorView(props) {
 
     return (
         <div className="donor-view">
-            <ProtectedDonorViewTitle
+            <PublicDonorViewTitle
                 context={context}
                 session={session}
                 href={href}
                 title={titleString}
             />
             <div className="view-content">
-                <ProtectedDonorViewHeader
+                <PublicDonorViewHeader
                     context={context}
                     session={session}
                     title={titleString}
                 />
-                <ProtectedDonorViewDataCards
+                <PublicDonorViewDataCards
                     context={context}
                     statisticValues={statisticValues}
                     isLoading={isLoading}
@@ -528,10 +522,10 @@ const ProtectedDonorView = React.memo(function ProtectedDonorView(props) {
  * Tab object for the FileView component, provides necessary information
  * to parent class, DefaultItemView
  */
-ProtectedDonorView.getTabObject = function (props) {
+PublicDonorView.getTabObject = function (props) {
     return {
         tab: <span>Donor Overview</span>,
         key: 'donor-overview',
-        content: <ProtectedDonorView {...props} />,
+        content: <PublicDonorView {...props} />,
     };
 };
