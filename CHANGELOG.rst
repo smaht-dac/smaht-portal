1f3a0c31b8cbd09dddadb1b403854a313bab1cdf
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2025-07-28 19:33:48 -0400

    SN Donor manifest embeds (#491)
    * Add revlinks, embeds, and manifest columns
    
    * Add revlink tests
    
    * Update changelog
    
    * Fix typo
    
    * Fix typos
    
    * Update workbook inserts

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -7,6 +7,15 @@ smaht-portal
 Change Log
 ----------
 
+0.193.0
+=======
+`PR 491: SN Donor manifest embeds <https://github.com/smaht-dac/smaht-portal/pull/491>`_
+
+* Create revlinks on ProtectedDonor and MedicalHistory for donor metadata items
+* Embed all donor metadata properties on ProtectedDonor
+* Create TSV columns in `metadata.py` for the Donor Manifest
+
+
 0.192.8
 =======
 `PR 493: AV Updates to QC metrics and Submission Status page <http://github.com/smaht-dac/smaht-portal/pull/493>`_
--- src/encoded/types/medical_history.py
+++ src/encoded/types/medical_history.py
@@ -30,7 +30,9 @@ class MedicalHistory(SubmittedItem):
     embedded_list = _build_medical_history_embedded_list()
 
     rev = {
-        "exposures": ("Exposure", "medical_history")
+        "exposures": ("Exposure", "medical_history"),
+        "diagnoses": ("Diagnosis", "medical_history"),
+        "medical_treatments": ("MedicalTreatment", "medical_history")
     }
 
     @calculated_property(
@@ -44,17 +46,33 @@ class MedicalHistory(SubmittedItem):
         },
     )
     def exposures(self, request: Request) -> Union[List[str], None]:
-        """Revlink of Exposures with category of Alcohol or Tobacco."""
         result = self.rev_link_atids(request, "exposures")
-        request_handler = RequestHandler(request = request)
-        categories = ["Alcohol", "Tobacco"]
-        valid_exposures = [ 
-            exp for exp in result
-            if get_property_value_from_identifier(
-                request_handler,
-                exp,
-                exposure_utils.get_category
-            ) in categories
-        ]
-        return valid_exposures or None
+        return result or None
 
+    @calculated_property(
+        schema={
+            "title": "Diagnoses",
+            "type": "array",
+            "items": {
+                "type": "string",
+                "linkTo": "Diagnosis",
+            },
+        },
+    )
+    def diagnoses(self, request: Request) -> Union[List[str], None]:
+        result = self.rev_link_atids(request, "diagnoses")
+        return result or None
+ 
+    @calculated_property(
+        schema={
+            "title": "Medical Treatments",
+            "type": "array",
+            "items": {
+                "type": "string",
+                "linkTo": "MedicalTreatment",
+            },
+        },
+    )
+    def medical_treatments(self, request: Request) -> Union[List[str], None]:
+        result = self.rev_link_atids(request, "medical_treatments")
+        return result or None
