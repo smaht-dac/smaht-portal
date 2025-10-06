8471766de241f96c479f7d1b7abea56cbe5739a1
utku-ozturk <utku.ozturk@karyasmd.com>
2025-10-06 19:50:35 +0300

    feat: update data matrix query URLs to include new status options

--- docs/public/data_matrix_comparison.jsx
+++ docs/public/data_matrix_comparison.jsx
@@ -12,7 +12,7 @@
                         key="data-matrix-benchmarking" // Required to prevent re-instantiation of component upon window resize & similar.
                         session={session} // Required - hooks in 'session' (boolean) from App.
                         query={{
-                            "url": "/data_matrix_aggregations/?type=File&sample_summary.studies=Benchmarking&dataset!=colo829blt_in_silico&dataset!=colo829_snv_indel_challenge_data&dataset!=mei_detection_challenge_data&dataset!=ipsc_snv_indel_challenge_data&status=released&status=public&status=restricted&status=public-restricted&limit=all",
+                            "url": "/data_matrix_aggregations/?type=File&sample_summary.studies=Benchmarking&dataset!=colo829blt_in_silico&dataset!=colo829_snv_indel_challenge_data&dataset!=mei_detection_challenge_data&dataset!=ipsc_snv_indel_challenge_data&status=open&status=open-early&status=open-network&status=protected&status=protected-early&status=protected-network&limit=all",
                             "columnAggFields": ["file_sets.libraries.assay.display_title", "sequencing.sequencer.platform"], //composite column
                             "rowAggFields": ["donors.display_title", "sample_summary.tissues", "dataset"], //multiple column
                         }}
@@ -52,7 +52,7 @@
                         key="data-matrix-production" // Required to prevent re-instantiation of component upon window resize & similar.
                         session={session} // Required - hooks in 'session' (boolean) from App.
                         query={{
-                            "url": "/data_matrix_aggregations/?type=File&sample_summary.studies=Production&status=released&status=public&status=restricted&status=public-restricted&limit=all",
+                            "url": "/data_matrix_aggregations/?type=File&sample_summary.studies=Production&status=open&status=open-early&status=open-network&status=protected&status=protected-early&status=protected-network&limit=all",
                             "columnAggFields": ["file_sets.libraries.assay.display_title", "sequencing.sequencer.platform"], //composite column
                             "rowAggFields": ["donors.display_title", "sample_summary.tissues"], //multiple column
                         }}
