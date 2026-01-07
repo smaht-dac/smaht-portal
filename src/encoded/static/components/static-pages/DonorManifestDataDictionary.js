65b4f6cf40b19c70754f9e1f8b3bf438131d381f
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-01-07 13:52:50 -0500

    - feat: update donor manifest diagram - style: update colors

--- src/encoded/static/components/static-pages/DonorManifestDataDictionary.js
+++ src/encoded/static/components/static-pages/DonorManifestDataDictionary.js
@@ -252,32 +252,36 @@ export const DonorManifestDataDictionary = () => {
                     <i>familyhistory.disease</i>).
                 </p>
 
-                <div className="diagram d-flex flex-column align-items-start">
+                <div className="diagram d-flex flex-column align-items-start mt-2">
                     <p className="mb-1">
                         Column Name Breakdown:{' '}
-                        <span className="category p-1">Category</span>.
-                        <span className="field p-1">Field</span>
+                        <span className="category p-1 fw-bold">Category</span>.
+                        <span className="field p-1 fw-bold">Field</span>
                     </p>
                     <img
                         src="/static/img/docs/donor_manifest_breakdown.png"
                         alt="Donor Manifest Breakdown"
                     />
                 </div>
-                <p className="mb-2">
+                <p className="my-3">
                     Note: All information fields except age, sex, and Hardy
                     scale are protected data under dbGaP. No protected
                     information is shown on this page.
                 </p>
             </div>
             <hr className="my-4"></hr>
-            <Select
-                value={selectedSchema}
-                placeholder="Select a item type or property from the Donor Metadata (e.g. Demographic)..."
-                onChange={(selectedItem) => {
-                    setSelectedSchema(selectedItem);
-                }}
-                options={options}
-            />
+            <div className="d-flex flex-column gap-2 align-items-start w-100">
+                <span className="fw-bold">Search:</span>
+                <Select
+                    className="w-100"
+                    value={selectedSchema}
+                    placeholder="Select a item type or property from the Donor Metadata (e.g. Demographic)..."
+                    onChange={(selectedItem) => {
+                        setSelectedSchema(selectedItem);
+                    }}
+                    options={options}
+                />
+            </div>
             {selectedSchema?.value && (
                 <div
                     className={`selected-schema schema-item ${selectedSchema.value} table-responsive`}>
