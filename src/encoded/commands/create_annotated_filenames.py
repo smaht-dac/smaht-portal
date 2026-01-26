37a41e5b2fa3920ec71e80bcf5a8153d70617c9f
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2026-01-26 14:30:02 -0500

    Restructure chain file formatting for non-unique codes

--- src/encoded/commands/create_annotated_filenames.py
+++ src/encoded/commands/create_annotated_filenames.py
@@ -130,7 +130,6 @@ def get_associated_items(
     reference_genome = get_reference_genome(file, request_handler)
     gene_annotations = get_gene_annotations(file, request_handler)
     donor_specific_assembly = get_donor_specific_assembly(file, request_handler)
-    import pdb; pdb.set_trace()
     target_assembly = get_target_assembly(file, request_handler)
     source_assembly = get_source_assembly(file, request_handler)
     if donor_specific_assembly:
@@ -236,18 +235,42 @@ def get_reference_genome(
     return get_item(file_utils.get_reference_genome(file), request_handler)
 
 
+def get_reference_genome_code(value: str, request_handler: RequestHandler):
+    """
+    Search Reference Genomes by code and title and return unique code for chain file output.
+    
+    NOTE: This relies on manual setting of ReferenceGenome `code` values internally. 
+    `title` for DSAs is submitter-provided and may be variable.
+    """
+    code_search = f"/search/?type=ReferenceGenome&code={value}"
+    title_search = f"/search/?type=ReferenceGenome&title={value}"
+    result = ff_utils.search_metadata(code_search, key=request_handler.auth_key) + ff_utils.search_metadata(title_search, key=request_handler.auth_key)
+    # If any of the reference genomes are DSAs, we can assume all of them are; use DSA value
+    if any([dsa_utils.is_donor_specific_assembly(ref) for ref in result]):
+        return DSA_INFO_VALUE
+    # Get unique code values from result
+    ref_code = []
+    for ref in result:
+        if (new_code := item_utils.get_code(ref)) not in ref_code:
+            ref_code.append(new_code)
+    # If there is more than one unique code in the returned reference genomes, return empty to raise error
+    if len(ref_code) != 1:
+        return ""
+    return ref_code[0]
+
+
 def get_target_assembly(
     file: Dict[str, Any], request_handler: RequestHandler
 ) -> Union[None, Dict[str, Any]]:
     """Get target assembly for file."""
-    return get_item(supp_file_utils.get_target_assembly(file), request_handler)
+    return get_reference_genome_code(supp_file_utils.get_target_assembly(file), request_handler)
 
 
 def get_source_assembly(
     file: Dict[str, Any], request_handler: RequestHandler
 ) -> Union[None, Dict[str, Any]]:
     """Get source assembly for file."""
-    return get_item(supp_file_utils.get_source_assembly(file), request_handler)
+    return get_reference_genome_code(supp_file_utils.get_source_assembly(file), request_handler)
 
 
 def get_gene_annotations(
@@ -900,6 +923,8 @@ def get_analysis(
         gene_annotation_code,
         transcript_info_code,
         chain_code,
+        target_assembly,
+        source_assembly,
         haplotype_code,
         file_extension,
         assay,
@@ -919,6 +944,8 @@ def get_analysis_errors(
     gene_annotation_code: str,
     transcript_info_code:  str,
     chain_code: str,
+    target_assembly: str,
+    source_assembly: str, 
     haplotype_code: str,
     file_extension: Dict[str, Any],
     assays: List[Dict[str, Any]],
@@ -940,8 +967,12 @@ def get_analysis_errors(
         elif file_format_utils.is_tsv_file(file_extension) and not transcript_info_code:
             errors.append("No gene or isoform code found")
     if file_format_utils.is_chain_file(file_extension):
+        if not target_assembly:
+            errors.append("Invalid target assembly value for chain conversion")
+        if not source_assembly:
+            errors.append("Invalid source assembly value for chain conversion")
         if not chain_code:
-            errors.append("No target or source assembly found for chain conversion")
+            errors.append("No chain code found")
     if CONSENSUS_DATA_CATEGORY in file_utils.get_data_category(file):
         if len(get_assay_categories(assays)) == 0:
             errors.append("No assay categories found.")
@@ -1086,14 +1117,8 @@ def get_chain_file_value(
     ) -> str:
     """Get genome conversion direction for chain files."""
     if file_format_utils.is_chain_file(file_extension):
-        target_value = ""
-        source_value = ""
         if target_assembly and source_assembly:
-            import pdb; pdb.set_trace()
-            target_value = DSA_INFO_VALUE if dsa_utils.is_donor_specific_assembly(target_assembly) else item_utils.get_code(target_assembly)
-            source_value = DSA_INFO_VALUE if dsa_utils.is_donor_specific_assembly(source_assembly) else item_utils.get_code(source_assembly)
-        if target_value and source_value:
-            return CHAIN_FILE_INFO_SEPARATOR.join([source_value,target_value])
+            return CHAIN_FILE_INFO_SEPARATOR.join([target_assembly,source_assembly])
     return ""
 
 
