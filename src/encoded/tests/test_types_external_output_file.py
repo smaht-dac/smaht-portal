a437ca1a7170f802e85dedbb6bb713daef084213
sarahgonicholson <sarah_nicholson@hms.harvard.edu>
2026-02-03 11:33:15 -0500

    Add test

---
+++ src/encoded/tests/test_types_external_output_file.py
@@ -0,0 +1,16 @@
+import pytest
+from webtest import TestApp
+
+from .utils import get_insert_identifier_for_item_type, get_item
+
+
+@pytest.mark.workbook
+def test_sample_sources_calc_prop(es_testapp: TestApp, workbook: None) -> None:
+    """Ensure sample_sources calc prop works."""
+    uuid=get_insert_identifier_for_item_type(es_testapp,"ExternalOutputFile")
+    eof=get_item(
+        es_testapp,
+        uuid,
+        collection='ExternalOutputFile',
+    )
+    assert len(eof.get("sample_sources",[])) == 1
