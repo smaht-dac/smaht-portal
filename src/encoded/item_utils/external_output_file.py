3b40052938518ae731028df2b0504bf447db51c1
aschroed <andrew_schroeder@hms.harvard.edu>
2026-02-05 09:34:20 -0500

    modified sample summary calcprop functions to produce for external output files

--- src/encoded/item_utils/external_output_file.py
+++ src/encoded/item_utils/external_output_file.py
@@ -24,3 +24,49 @@ def get_donors(
     return list(set(get_property_values_from_identifiers(
         request_handler, get_tissues(properties), tissue_utils.get_donor
     )))
+
+
+def get_tissue_category(
+    properties: Dict[str, Any],
+    request_handler: RequestHandler
+) -> List[str]:
+    """Get tissue category associated with external output file.
+        can be multiple tissues if more than one category return ? multiple"""
+    tissue_ids = get_tissues(properties)
+    tissues = request_handler.get_items(tissue_ids)
+    categories = set()
+    for tissue in tissues:
+        categories.add(tissue_utils.get_category(tissue, request_handler))
+    if len(categories) == 1:
+        return list(categories)
+    if len(categories) > 1:
+        # TODO: is this what we want to do or just return all?
+        return ['Multiple']
+    return []
+
+
+def get_tissue_type(
+    properties: Dict[str, Any],
+    request_handler: RequestHandler
+) -> List[str]:
+    """Get tissue types associated with external output file.
+        can be multiple tissue types if more than one tissue"""
+    tissue_ids = get_tissues(properties)
+    tissues = request_handler.get_items(tissue_ids)
+    types = set()
+    for tissue in tissues:
+        types.add(tissue_utils.get_category(tissue, request_handler))
+    # TODO: is this what we want to do or return a single value if over some number?
+    return list(set(types))
+
+
+
+def get_uberon_ids(
+    properties: Dict[str, Any],
+    request_handler: RequestHandler
+) -> List[str]:
+    """Get uberon ids associated with external output file."""
+    return list(set(get_property_values_from_identifiers(
+        request_handler, get_tissues(properties), tissue_utils.get_uberon_id
+    )))
+
--- src/encoded/types/external_output_file.py
+++ src/encoded/types/external_output_file.py
@@ -8,7 +8,9 @@ from .file import CalcPropConstants
 
 from ..item_utils import (
     file as file_utils,
-    external_output_file as eof_utils
+    external_output_file as eof_utils,
+    tissue as tissue_utils,
+    item as item_utils,
 )
 from ..item_utils.utils import (
     get_property_value_from_identifier,
@@ -36,6 +38,7 @@ class ExternalOutputFile(SubmittedFile):
     item_type = "external_output_file"
     schema = load_schema("encoded:schemas/external_output_file.json")
     embedded_list = _build_external_output_file_embedded_list()
+
     @calculated_property(schema=CalcPropConstants.SAMPLE_SOURCES_SCHEMA)
     def sample_sources(
        self,
@@ -48,7 +51,7 @@ class ExternalOutputFile(SubmittedFile):
         else:
             result = eof_utils.get_tissues(self.properties)
         return result or None
-    
+
     @calculated_property(schema=CalcPropConstants.DONORS_SCHEMA)
     def donors(
        self,
@@ -62,3 +65,58 @@ class ExternalOutputFile(SubmittedFile):
             request_handler = RequestHandler(request=request)
             result = eof_utils.get_donors(self.properties, request_handler)
         return result or None
+
+    def _get_sample_summary_fields(
+        self, request_handler: RequestHandler, file_properties: Dict[str, Any]
+    ) -> Dict[str, Any]:
+        """Get sample summary for display on file overview page."""
+        to_include = {
+            CalcPropConstants.SAMPLE_SUMMARY_DONOR_IDS: get_property_values_from_identifiers(
+                request_handler,
+                eof_utils.get_donors(file_properties, request_handler),
+                item_utils.get_external_id,
+            ),
+            CalcPropConstants.SAMPLE_SUMMARY_CATEGORY: eof_utils.get_tissue_category(file_properties, request_handler),
+            CalcPropConstants.SAMPLE_SUMMARY_TISSUES: eof_utils.get_tissue_type(file_properties, request_handler),
+            CalcPropConstants.SAMPLE_SUMMARY_TISSUE_SUBTYPES: get_property_values_from_identifiers(
+                request_handler,
+                eof_utils.get_uberon_ids(file_properties, request_handler),
+                item_utils.get_display_title,
+            ),
+            CalcPropConstants.SAMPLE_SUMMARY_TISSUE_DETAILS: get_property_values_from_identifiers(
+                request_handler,
+                eof_utils.get_tissues(file_properties),
+                tissue_utils.get_location,
+            ),
+            # CalcPropConstants.SAMPLE_SUMMARY_SAMPLE_NAMES: get_property_values_from_identifiers(
+            #     request_handler,
+            #     file_utils.get_samples(file_properties, request_handler),
+            #     functools.partial(
+            #         sample_utils.get_sample_names, request_handler=request_handler
+            #     ),
+            # ),
+            # CalcPropConstants.SAMPLE_SUMMARY_SAMPLE_DESCRIPTIONS:
+            #     get_property_values_from_identifiers(
+            #         request_handler,
+            #         file_utils.get_samples(file_properties, request_handler),
+            #         functools.partial(
+            #             sample_utils.get_sample_descriptions,
+            #             request_handler=request_handler,
+            #         ),
+            #     ),
+            # CalcPropConstants.SAMPLE_SUMMARY_STUDIES: get_property_values_from_identifiers(
+            #     request_handler,
+            #     file_utils.get_samples(file_properties, request_handler),
+            #     functools.partial(
+            #         sample_utils.get_studies, request_handler=request_handler
+            #     ),
+            # ),
+            # CalcPropConstants.SAMPLE_SUMMARY_ANALYTES: get_property_values_from_identifiers(
+            #     request_handler,
+            #     file_utils.get_analytes(file_properties, request_handler),
+            #     analyte_utils.get_molecule,
+            # ),
+        }
+        result = {key: value for key, value in to_include.items() if value}
+        return result
+
