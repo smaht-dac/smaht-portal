5a243bc08510a43790e00f71dd1c773bb4b1a6cd
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-05-15 10:24:39 -0400

    feat: add button for P25 Data freeze

--- src/encoded/static/components/static-pages/HomePage/NotificationsPanel.js
+++ src/encoded/static/components/static-pages/HomePage/NotificationsPanel.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { DataReleaseTracker } from './DataReleaseTracker';
 import { AnnouncementsSection } from './AnnouncementsSection';
 
+const FILTERD_SOMATIC_VARIANTS_URL =
+    '/browse/?analysis_details=Filtered&data_category=Somatic Variant Calls&donors.donor_groups=First 25 Donors [P25]&type=File';
+
 /**
  * NotificationsPanel component displays a panel containing the data release
  * tracker, the announcements section, and other relevant links/information.
@@ -11,6 +14,20 @@ export const NotificationsPanel = (props) => {
     const { session } = props;
     return (
         <div className="notifications-panel container">
+            <div className="section data-freeze">
+                <h3 className="section-header">
+                    P25 Data Freeze
+                    <i
+                        className="icon icon-fw icon-info-circle fas ms-1"
+                        data-tip="P25 represents the first 25 donors in the P25 cohort. The P25 cohort is a subset of the P25 cohort, which is a subset of the first 25 donors in the cohort."></i>
+                </h3>
+                <a className="p25-link" href={FILTERD_SOMATIC_VARIANTS_URL}>
+                    <span>
+                        <i className="icon icon-fw icon-filter fas me-1"></i>
+                        Filtered Somatic Variants
+                    </span>
+                </a>
+            </div>
             <DataReleaseTracker session={session} />
             <AnnouncementsSection />
             <div className="about-consortium section">
