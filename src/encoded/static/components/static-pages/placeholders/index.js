fe24754b1bffae17d3079e1fd48af3105c2afb3c
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-05-26 14:04:31 -0400

    feat: publication page layout

--- src/encoded/static/components/static-pages/placeholders/index.js
+++ src/encoded/static/components/static-pages/placeholders/index.js
@@ -34,6 +34,8 @@ import { SubmissionDataDictionary } from '../SubmissionDataDictionary';
 import { DonorManifestDataDictionary } from '../DonorManifestDataDictionary';
 import { PipelineDocsNavigation } from '../PipelineDocsNavigation';
 import { DataMatrixComparisonTabs } from '../components/DataMatrixComparisonTabs';
+import { PublicationCollectionsNavigation } from '../PublicationCollectionsNavigation';
+import { PublicationCollection } from '../PublicationCollection';
 
 /**
  * Any placeholder(s) used in a StaticSection _must_ get imported here
@@ -65,6 +67,8 @@ const placeholders = {
     SubmissionDataDictionary,
     DonorManifestDataDictionary,
     PipelineDocsNavigation,
+    PublicationCollectionsNavigation,
+    PublicationCollection,
 };
 
 export const replaceString = memoize(
