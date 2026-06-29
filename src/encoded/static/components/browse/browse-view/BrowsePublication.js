f5feb77b7506dff9abe683072dcbf39586950fc8
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-06-29 16:33:43 -0400

    fix: remove unused code

--- src/encoded/static/components/browse/browse-view/BrowsePublication.js
+++ src/encoded/static/components/browse/browse-view/BrowsePublication.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import * as _ from 'underscore';
 import { BrowseViewControllerWithSelections } from '../../static-pages/components/TableControllerWithSelections';
 import { BrowsePublicationSearchTable } from '../components/PublicationSearchTable';
