65bf4d7361825b928949b29ff2fe218a9f443959
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-07-09 12:50:36 -0400

    refactor: remove unused code

--- src/encoded/static/components/browse/browse-view/BrowseDonor.js
+++ src/encoded/static/components/browse/browse-view/BrowseDonor.js
@@ -131,23 +131,21 @@ export const BrowseDonorBody = (props) => {
 
     return (
         <DonorDataProvider>
-            <>
-                {showRedirectBanner && <RedirectBanner href={props?.href} />}
-                <BrowseDonorVizWrapper {...props} mapping="donor" />
-                <hr />
-                <BrowseViewControllerWithSelections {...props}>
-                    <BrowseDonorSearchTable />
-                </BrowseViewControllerWithSelections>
-                {context?.total === 0 && (
-                    <NoResultsBrowseModal
-                        type="donor"
-                        context={context}
-                        href={href}
-                        userDownloadAccess={userDownloadAccess}
-                        isAccessResolved={isAccessResolved}
-                    />
-                )}
-            </>
+            {showRedirectBanner && <RedirectBanner href={props.href} />}
+            <BrowseDonorVizWrapper {...props} mapping="donor" />
+            <hr />
+            <BrowseViewControllerWithSelections {...props}>
+                <BrowseDonorSearchTable />
+            </BrowseViewControllerWithSelections>
+            {context?.total === 0 && (
+                <NoResultsBrowseModal
+                    type="donor"
+                    context={context}
+                    href={href}
+                    userDownloadAccess={userDownloadAccess}
+                    isAccessResolved={isAccessResolved}
+                />
+            )}
         </DonorDataProvider>
     );
 };
