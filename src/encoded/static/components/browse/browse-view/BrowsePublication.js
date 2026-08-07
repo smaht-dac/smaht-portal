f1860c07f3bc1da625c8751feec275cbbefed81c
Cesar Ferreyra-Mansilla <crf85@cornell.edu>
2026-08-07 00:09:46 -0400

    feat: add publication header section

--- src/encoded/static/components/browse/browse-view/BrowsePublication.js
+++ src/encoded/static/components/browse/browse-view/BrowsePublication.js
@@ -7,6 +7,22 @@ import { BrowsePublicationSearchTable } from '../components/PublicationSearchTab
 export const BrowsePublicationBody = (props) => {
     return (
         <div className="browse-publication-body text-gray-70">
+            <div className="publication-collections-breadcrumb">
+                <span>SMaHT Data</span>
+                <span className="crumb-divider">|</span>
+                <span>Publications</span>
+            </div>
+            <div className="publication-collections-heading-row">
+                <h2 className="publication-collections-heading">
+                    <b>Browse</b> all <b>SMaHT Publications</b>
+                </h2>
+                <a
+                    className="publication-collections-search-btn"
+                    href="/publication-collections">
+                    <i className="icon icon-bookmark fas"></i>
+                    Browse all Collections
+                </a>
+            </div>
             <BrowseViewControllerWithSelections {...props}>
                 <BrowsePublicationSearchTable {...props} />
             </BrowseViewControllerWithSelections>
